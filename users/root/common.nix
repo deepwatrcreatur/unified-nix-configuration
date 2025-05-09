16909319cc95f05555ffadfc285af43e1413a5b8
Anwer Khan <deepwatrcreatur@gmail.com>
2025-05-09 17:57:05 -0400

    changed directory name for greater clarity

--- users/root/common.nix
+++ users/root/common.nix
@@ -3,8 +3,8 @@
 { config, pkgs, lib, ... }:
 {
   imports = [
-    ../modules/home/fish-shared.nix
-    ../modules/home/git.nix
+    ../modules/home-manager/fish-shared.nix
+    ../modules/home-manager/git.nix
    ];
   
   home.username = "root";
