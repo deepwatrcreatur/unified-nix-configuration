17c1661194ec21310b1b7e515772d0b942d8fae7
Anwer Khan <deepwatrcreatur@gmail.com>
2025-05-12 06:26:27 -0400

    moved some configuration to common-home.nix

--- modules/home-manager/common-home.nix
+++ modules/home-manager/common-home.nix
@@ -3,17 +3,23 @@
 {
   imports = [
     ./helix.nix
-    # add other shared user modules here
+    ./fish-shared.nix
   ];
 
   home.packages = with pkgs; [
     lsd
     bat
     fzf
-    starship
     fastfetch
-    neovim
-        
+    neovim    
   ];
-}
 
+  # Copy .terminfo files into place
+  home.file.".terminfo" = {
+    source = ../../modules/home-manager/terminfo;
+    recursive = true;
+  };
+
+  programs.starship.enable = true;
+  programs.home-manager.enable = true;
+}
