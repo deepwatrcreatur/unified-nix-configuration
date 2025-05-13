230cebaa739d5143825fc9a37d4ce8045456929e
Anwer Khan <deepwatrcreatur@gmail.com>
2025-05-13 02:56:37 -0400

    deactivating more for debugging

--- flake-parts/users/deepwatrcreatur.nix
+++ flake-parts/users/deepwatrcreatur.nix
@@ -1,17 +1,17 @@
 # flake-parts/users/deepwatrcreatur.nix
 { inputs, ... }:
 {
-  perSystem = { config, pkgs, ... }: {
-    # Standalone Home Manager config (optional, for non-NixOS/darwin)
-    homeConfigurations."deepwatrcreatur@pve-strix" = inputs.home-manager.lib.homeManagerConfiguration {
-      pkgs = pkgs;
-      extraSpecialArgs = { };
-      modules = [
-        ./../../users/deepwatrcreatur/common.nix
-        ./../../users/deepwatrcreatur/hosts/pve-strix.nix
-      ];
-    };
-  };
+  #perSystem = { config, pkgs, ... }: {
+  #  # Standalone Home Manager config (optional, for non-NixOS/darwin)
+  #  homeConfigurations."deepwatrcreatur@pve-strix" = inputs.home-manager.lib.homeManagerConfiguration {
+  #    pkgs = pkgs;
+  #    extraSpecialArgs = { };
+  #    modules = [
+  #      ./../../users/deepwatrcreatur/common.nix
+  #      ./../../users/deepwatrcreatur/hosts/pve-strix.nix
+  #    ];
+  #  };
+  #};
 
   # Home Manager as a module for NixOS/darwin hosts
   flake = {
