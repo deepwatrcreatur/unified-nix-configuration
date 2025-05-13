f87a9723abab64eedb1d939070e2f5083a5e6a40
Anwer Khan <deepwatrcreatur@gmail.com>
2025-05-13 02:58:20 -0400

    deactivating more for debugging

--- users/root/common.nix
+++ users/root/common.nix
@@ -1,15 +1,15 @@
 { inputs, ... }:
 {
   # Only for standalone Home Manager configs, if you want them
-  perSystem = { config, pkgs, ... }: {
-    homeConfigurations."root@pve-strix" = inputs.home-manager.lib.homeManagerConfiguration {
-      pkgs = pkgs;
-      extraSpecialArgs = { };
-      modules = [
-        ../../users/root/common.nix
+  #perSystem = { config, pkgs, ... }: {
+  #  homeConfigurations."root@pve-strix" = inputs.home-manager.lib.homeManagerConfiguration {
+  #    pkgs = pkgs;
+  #    extraSpecialArgs = { };
+  #    modules = [
+  #      ../../users/root/common.nix
         # ...other host-specific modules...
-      ];
-    };
-  };
+  #    ];
+  #  };
+  #};
 }
 
