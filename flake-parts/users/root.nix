fc4885e4fac19e505127e639a66664e1e7478cdd
Anwer Khan <deepwatrcreatur@gmail.com>
2025-05-13 02:56:37 -0400

    deactivating more for debugging

--- flake-parts/users/root.nix
+++ flake-parts/users/root.nix
@@ -1,15 +1,15 @@
 # flake-parts/users/root.nix
 { inputs, ... }:
 {
-  perSystem = { config, pkgs, ... }: {
-    homeConfigurations."root@pve-strix" = inputs.home-manager.lib.homeManagerConfiguration {
-      pkgs = pkgs;
-      extraSpecialArgs = { };
-      modules = [
-        ../../users/root/common.nix
-        # ...other host-specific modules...
-      ];
-    };
-  };
+#  perSystem = { config, pkgs, ... }: {
+#    homeConfigurations."root@pve-strix" = inputs.home-manager.lib.homeManagerConfiguration {
+#      pkgs = pkgs;
+#      extraSpecialArgs = { };
+#      modules = [
+#        ../../users/root/common.nix
+#        # ...other host-specific modules...
+#      ];
+#    };
+#  };
 }
 
