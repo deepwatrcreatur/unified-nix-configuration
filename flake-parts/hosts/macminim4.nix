5603a720d9f28e37a7388e9bfe467b4ebc3128c3
Anwer Khan <deepwatrcreatur@gmail.com>
2025-05-13 03:51:16 -0400

    trying to correct home-manager confict with system config

--- flake-parts/hosts/macminim4.nix
+++ flake-parts/hosts/macminim4.nix
@@ -2,21 +2,16 @@
 {
   flake.darwinConfigurations.macminim4 = inputs.nix-darwin.lib.darwinSystem {
     system = "aarch64-darwin";
-    specialArgs = { inherit inputs; };
     modules = [
-      #../../hosts/macminim4/default.nix
-      #../../hosts/common-darwin.nix
-      # sops-nix if needed
+      # ...
       inputs.home-manager.darwinModules.home-manager
       {
         home-manager = {
-          #useGlobalPkgs = true;
-          #useUserPackages = false;
-          #extraSpecialArgs = { };
           users.deepwatrcreatur = {
             imports = [
-              #../../users/deepwatrcreatur/common.nix
-              #../../users/deepwatrcreatur/hosts/macminim4.nix
+              ../../users/deepwatrcreatur/common.nix
+              ../../users/deepwatrcreatur/common-darwin.nix
+              ../../users/deepwatrcreatur/hosts/macminim4.nix
             ];
           };
         };
