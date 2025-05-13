abd08eb6292d7f489cace047aedb4ac15c5cd182
Anwer Khan <deepwatrcreatur@gmail.com>
2025-05-13 02:35:37 -0400

    restoring full config after debugging test

--- flake.nix
+++ flake.nix
@@ -1,6 +1,26 @@
-
 {
-  description = "Test flake";
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/24.05";
-  outputs = { self, nixpkgs }: {};
+  description = "Multi-system Nix configurations (NixOS, nix-darwin, Home Manager)";
+
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/24.05";
+    nix-darwin.url = "github:LnL7/nix-darwin";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    sops-nix.url = "github:Mic92/sops-nix";
+    sops-nix.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        ./flake-parts/hosts/macminim4.nix
+        ./flake-parts/hosts/homeserver.nix
+        #./flake-parts/hosts/inference1.nix
+        ./flake-parts/users/deepwatrcreatur.nix
+        ./flake-parts/users/root.nix
+      ];
+
+    };
 }
