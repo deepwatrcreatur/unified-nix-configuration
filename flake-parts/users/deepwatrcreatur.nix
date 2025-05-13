8866e33197621121d256741251df0fd01ae25419
Anwer Khan <deepwatrcreatur@gmail.com>
2025-05-13 03:51:16 -0400

    trying to correct home-manager confict with system config

--- flake-parts/users/deepwatrcreatur.nix
+++ flake-parts/users/deepwatrcreatur.nix
@@ -1,20 +1,14 @@
-# flake-parts/users/deepwatrcreatur.nix
 { inputs, ... }:
 {
-  #perSystem = { config, pkgs, ... }: {
-  #  # Standalone Home Manager config (optional, for non-NixOS/darwin)
-  #  homeConfigurations."deepwatrcreatur@pve-strix" = inputs.home-manager.lib.homeManagerConfiguration {
-  #    pkgs = pkgs;
-  #    extraSpecialArgs = { };
-  #    modules = [
-  #      ./../../users/deepwatrcreatur/common.nix
-  #      ./../../users/deepwatrcreatur/hosts/pve-strix.nix
-  #    ];
-  #  };
-  #};
-
-  # Home Manager as a module for NixOS/darwin hosts
-  flake = {
+  perSystem = { config, pkgs, ... }: {
+    # Only for Linux!
+    homeConfigurations."deepwatrcreatur@pve-strix" = inputs.home-manager.lib.homeManagerConfiguration {
+      pkgs = pkgs;
+      modules = [
+        ../../users/deepwatrcreatur/common.nix
+        #../../users/deepwatrcreatur/common-linux.nix
+        #../../users/deepwatrcreatur/hosts/pve-strix.nix
+      ];
+    };
   };
 }
-
