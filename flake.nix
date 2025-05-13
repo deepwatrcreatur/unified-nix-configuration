25c2ea6ca3ee7bedd8ab6fc07ee5f57c32819b40
Anwer Khan <deepwatrcreatur@gmail.com>
2025-05-13 01:40:44 -0400

    reactivating homeserver config

--- flake.nix
+++ flake.nix
@@ -16,7 +16,7 @@
     flake-parts.lib.mkFlake { inherit inputs; } {
       imports = [
         ./flake-parts/hosts/macminim4.nix
-        #./flake-parts/hosts/homeserver.nix
+        ./flake-parts/hosts/homeserver.nix
         #./flake-parts/hosts/inference1.nix
         ./flake-parts/users/deepwatrcreatur.nix
         ./flake-parts/users/root.nix
