10bea3070dcc03c4f8a4fd364025414b4fbe4a61
Anwer Khan <deepwatrcreatur@gmail.com>
2025-05-13 01:22:25 -0400

    trying to prevent merging of home manager output as a module instead of system output

--- flake-parts/hosts/macminim4.nix
+++ flake-parts/hosts/macminim4.nix
@@ -1,6 +1,6 @@
 { inputs, ... }:
 {
-  systems.darwinConfigurations.macminim4 = {
+  flake.darwinConfigurations.macminim4 = inputs.nix-darwin.lib.darwinSystem {
     system = "aarch64-darwin";
     specialArgs = { inherit inputs; };
     modules = [
@@ -24,4 +24,3 @@
     ];
   };
 }
-
