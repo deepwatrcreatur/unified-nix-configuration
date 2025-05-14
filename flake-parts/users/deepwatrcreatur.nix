74365b02c536fae26cbc46c753f20ed18c9e8943
Anwer Khan <deepwatrcreatur@gmail.com>
2025-05-14 01:03:53 -0400

    trying to limit standalone home manager to linux hosts

--- flake-parts/users/deepwatrcreatur.nix
+++ flake-parts/users/deepwatrcreatur.nix
@@ -12,15 +12,16 @@ in
 {
   perSystem = { config, pkgs, system, ... }: {
     homeConfigurations =
-      pkgs.lib.mkIf (system == "x86_64-linux") (
-        pkgs.lib.genAttrs standaloneHosts (host: 
+      pkgs.lib.mkIf (pkgs.stdenv.isLinux) (
+        pkgs.lib.genAttrs standaloneHosts (host:
           inputs.home-manager.lib.homeManagerConfiguration {
             pkgs = pkgs;
-            modules = [
-              ../../users/deepwatrcreatur/common.nix
-              ../../users/deepwatrcreatur/common-linux.nix
-              ../../users/deepwatrcreatur/hosts/${host}.nix
-            ];
+            modules =
+              [
+                ../../users/deepwatrcreatur/common.nix
+                ../../users/deepwatrcreatur/hosts/${host}.nix
+              ]
+              ++ (if pkgs.stdenv.isLinux then [ ../../users/deepwatrcreatur/common-linux.nix ] else []);
           }
         )
       );
