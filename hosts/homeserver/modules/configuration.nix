1bee0a92d7555dbd5651dc2684e95cff2f4d07a8
Anwer Khan <deepwatrcreatur@gmail.com>
2025-05-14 01:50:19 -0400

    correcting systemState: reverting to 24.11

--- hosts/homeserver/modules/configuration.nix
+++ hosts/homeserver/modules/configuration.nix
@@ -204,6 +204,6 @@
   i18n.defaultLocale = "en_US.UTF-8";
   i18n.supportedLocales = [ "en_US.UTF-8/UTF-8" ]; # Optional, ensures generation
     
-  system.stateVersion = "25.05";
+  system.stateVersion = "24.11";
 }
 
