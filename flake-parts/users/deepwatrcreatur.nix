2ff3e2d1380977049c71c947df96f2358cf311ef
Anwer Khan <deepwatrcreatur@gmail.com>
2025-05-14 00:55:29 -0400

    dynamic configuration including all standalone home manager for all linux hosts

--- flake-parts/users/deepwatrcreatur.nix
+++ flake-parts/users/deepwatrcreatur.nix
@@ -1,17 +1,28 @@
 { inputs, ... }:
+let
+  standaloneHosts = [
+    "pve-strix"
+    "homeserver"
+    #"ansible"
+    #"inference1"
+    #"inference2"
+    #"inference3"
+  ];
+in
 {
   perSystem = { config, pkgs, system, ... }: {
     homeConfigurations =
-      pkgs.lib.mkIf (system == "x86_64-linux") {
-        "deepwatrcreatur@pve-strix" = inputs.home-manager.lib.homeManagerConfiguration {
-          pkgs = pkgs;
-          modules = [
-            ../../users/deepwatrcreatur/common.nix
-            ../../users/deepwatrcreatur/common-linux.nix
-            ../../users/deepwatrcreatur/hosts/pve-strix.nix
-          ];
-        };
-      };
+      pkgs.lib.mkIf (system == "x86_64-linux") (
+        pkgs.lib.genAttrs standaloneHosts (host: 
+          inputs.home-manager.lib.homeManagerConfiguration {
+            pkgs = pkgs;
+            modules = [
+              ../../users/deepwatrcreatur/common.nix
+              ../../users/deepwatrcreatur/common-linux.nix
+              ../../users/deepwatrcreatur/hosts/${host}.nix
+            ];
+          }
+        )
+      );
   };
 }
-
