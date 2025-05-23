fc83dcff5bbc1a0ea0c0d2af7d7dd6dd9cdfe154
Anwer Khan <deepwatrcreatur@gmail.com>
2025-05-22 22:24:17 -0400

    changed structure of nix-mount trying for compatibility with required format

--- modules/nix-darwin/nix-mount.nix
+++ modules/nix-darwin/nix-mount.nix
@@ -1,11 +1,9 @@
-# modules/nix-mount.nix
+# modules/nix-darwin/nix-mount.nix
 { config, lib, ... }:
 let
-  # Define an option for the UUID
   cfg = config.custom.nix-mount;
 in
 {
-  # Declare a configuration option for the UUID
   options.custom.nix-mount = {
     uuid = lib.mkOption {
       type = lib.types.str;
@@ -14,25 +12,21 @@ in
     };
   };
 
-  # Define the launchd agent
   config = {
-    # Ensure /nix is in synthetic.conf
     environment.etc."synthetic.conf".text = ''
       nix
     '';
 
     launchd.agents.nix-mount = {
       enable = true;
-      config = {
-        ProgramArguments = [
-          "/bin/sh"
-          "-c"
-          "/usr/sbin/diskutil mount -mountPoint /nix ${cfg.uuid}"
-        ];
-        Label = "com.nix.mount";
-        RunAtLoad = true;
-        KeepAlive = false;
-      };
+      ProgramArguments = [
+        "/bin/sh"
+        "-c"
+        "/usr/sbin/diskutil mount -mountPoint /nix ${cfg.uuid}"
+      ];
+      Label = "com.nix.mount";
+      RunAtLoad = true;
+      KeepAlive = false;
     };
   };
 }
