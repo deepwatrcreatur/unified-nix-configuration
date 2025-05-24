06e082d44a58c4562f1851ff5b606eb8faec852d
Anwer Khan <deepwatrcreatur@gmail.com>
2025-05-24 01:35:15 -0400

    add ghostty theme to nix store

--- modules/home-manager/ghostty/default.nix
+++ modules/home-manager/ghostty/default.nix
@@ -1,14 +1,25 @@
 { pkgs, lib, ... }:
 
 {
-  # the darwin nix package is marked as broken
+  # Install Ghostty on Linux
   home.packages = lib.optionals pkgs.stdenv.isLinux [
-    pkgs.ghostty 
+    pkgs.ghostty
   ];
 
-  # Manage the ghostty configuration file
+  # Manage the Ghostty configuration file
   xdg.configFile."ghostty/config" = {
     source = ./config;
   };
-}
 
+  # Copy the Sugarplum theme to the themes folder
+  xdg.configFile."ghostty/themes/Sugarplum" = {
+    source = ./themes/Sugarplum;
+  };
+
+  # Ensure the themes folder exists
+  home.activation = {
+    createGhosttyThemesFolder = lib.hm.dag.entryAfter ["writeBoundary"] ''
+      $DRY_RUN_CMD mkdir -p $HOME/.config/ghostty/themes
+    '';
+  };
+}
