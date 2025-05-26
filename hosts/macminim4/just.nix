28e7f89d40913bdf93b28588eba514a052d2428f
Anwer Khan <deepwatrcreatur@gmail.com>
2025-05-26 03:05:12 -0400

    corrected missing import of just.nix and extra input causing error

--- hosts/macminim4/just.nix
+++ hosts/macminim4/just.nix
@@ -3,7 +3,7 @@
 {
   # Ensure just and nh are installed for the user
   home-manager.users.deepwatrcreatur = {
-    home.packages = [ pkgs.just inputs.nh.packages.aarch64-darwin.default ];
+    home.packages = [ pkgs.just ];
 
     # Reference the external justfile in the same directory
     home.file.".justfile".source = ./justfile;
