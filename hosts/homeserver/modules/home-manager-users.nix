53a8dd7c34c2535579cd0523ae778d4b0f57f09e
Anwer Khan <deepwatrcreatur@gmail.com>
2025-05-31 01:02:19 -0400

    corrected file paths reaching out of repo

--- hosts/homeserver/modules/home-manager-users.nix
+++ hosts/homeserver/modules/home-manager-users.nix
@@ -5,15 +5,15 @@
     useUserPackages = true;
     users.deepwatrcreatur = {
       imports = [
-        ../../../../users/deepwatrcreatur
-        ../../../../users/deepwatrcreatur/hosts/homeserver.nix
-        ../../../../modules/home-manager
+        ../../../users/deepwatrcreatur
+        ../../../users/deepwatrcreatur/hosts/homeserver.nix
+        ../../../modules/home-manager
       ];
     };
     users.root = {
       imports = [
-        ../../../../users/root/hosts/homeserver/default.nix
-        ../../../../modules/home-manager
+        ../../../users/root/hosts/homeserver/default.nix
+        ../../../modules/home-manager
       ];
     };
   };
