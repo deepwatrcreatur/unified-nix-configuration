ba799c6a4e792bc2e76b83204796b011e9da5447
Anwer Khan <deepwatrcreatur@gmail.com>
2025-06-01 01:21:05 -0400

    provide symlink for cargo command

--- modules/home-manager/cargo-binstall.nix
+++ modules/home-manager/cargo-binstall.nix
@@ -1,9 +1,18 @@
 # modules/home-manager/cargo-binstall.nix
 { pkgs, ... }:
 
+let
+  # Create a derivation that provides a 'cargo' binary which points to
+  # the cargo from pkgs.rustc. This ensures 'cargo' is in your path directly.
+  myCargo = pkgs.runCommand "my-cargo-bin" {} ''
+    mkdir -p $out/bin
+    ln -s ${pkgs.rustc}/bin/cargo $out/bin/cargo
+  '';
+
+in
 {
   home.packages = [
-    pkgs.rustc # Provides `cargo`
+    myCargo # This now explicitly provides the `cargo` command
     pkgs.cargo-binstall
   ];
 
