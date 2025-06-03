3fb55b01b96fea6140b248f3fe45009eb39596b1
Anwer Khan <deepwatrcreatur@gmail.com>
2025-06-03 00:45:03 -0400

    significant reorganization of flake using imported modules, added overlay to build helix from source

--- modules/home-manager/helix/default.nix
+++ modules/home-manager/helix/default.nix
@@ -1,16 +1,19 @@
-{ config, pkgs, ... }:
+# modules/home-manager/helix-config.nix
+{ config, pkgs, lib, ... }: 
+
 {
   programs.helix = {
     enable = true;
+    # Use the custom-built package from the overlay
+    package = pkgs.helix-from-src;
     defaultEditor = true;
-    extraPackages = with pkgs; [
+    extraPackages = with pkgs; [ 
       nil
       nixd
       nixpkgs-fmt
       elixir-ls
     ];
-    settings = import ./settings.nix;
+    settings = import ./settings.nix; 
     languages = import ./languages.nix { inherit pkgs; };
   };
 }
-
