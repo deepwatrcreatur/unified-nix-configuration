9ddb322687d0121b8b6fab0b7316956076cbe271
Anwer Khan <deepwatrcreatur@gmail.com>
2025-06-05 09:39:17 -0400

    correcting imports for pve-strix home manager modules

--- flake.nix
+++ flake.nix
@@ -5,14 +5,8 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
-    nix-darwin.url = "github:LnL7/nix-darwin/master";
-    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
-
-    home-manager.url = "github:nix-community/home-manager";
+    home-manager.url = "github:nix-community/home-manager"; 
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
-
-    sops-nix.url = "github:Mic92/sops-nix";
-    sops-nix.inputs.nixpkgs.follows = "nixpkgs";
 
     helix.url = "github:helix-editor/helix";
     helix.inputs.nixpkgs.follows = "nixpkgs";
@@ -20,19 +14,22 @@
 
   outputs = inputs@{ ... }:
   let
-    lib = inputs.nixpkgs.lib;
+    # Standard library from nixpkgs - this is the "pure" lib
+    nixpkgsLib = inputs.nixpkgs.lib;
+
     commonNixpkgsConfig = {
       allowUnfree = true;
     };
+    commonOverlays = [];
 
-    commonOverlays = [
-    ]; 
+    # SpecialArgs for NixOS and Darwin SYSTEM modules.
+    # These modules can safely receive the pure nixpkgsLib.
+    systemSpecialArgs = { inherit inputs; lib = nixpkgsLib; };
 
-    # Common specialArgs for NixOS, Darwin, and Home Manager systems
-    # Ensures modules can access flake inputs.
-    commonSpecialArgs = { inherit inputs lib; };
-
-    # Helper to import all .nix files from a directory as module paths.
+    # SpecialArgs specifically for HOME MANAGER modules.
+    # We only pass 'inputs'. Home Manager will provide its own 'lib' and 'config.lib'.
+    homeManagerModuleArgs = { inherit inputs; };
+# Helper to import all .nix files from a directory as module paths.
     importAllModulesInDir = dir:
       let
         items = builtins.readDir dir;
@@ -40,27 +37,29 @@
         nixFileNames = lib.attrNames (lib.filterAttrs isNixFile items);
       in
         map (fileName: dir + "/${fileName}") nixFileNames;
-
   in
   {
+    # --- Home Manager Configurations (Standalone) ---
     homeConfigurations = {
       proxmox-root = inputs.home-manager.lib.homeManagerConfiguration {
         pkgs = import inputs.nixpkgs {
-          system = "x86_64-linux";
+          system = "x86_64-linux"; 
           config = commonNixpkgsConfig;
           overlays = commonOverlays;
         };
-        extraSpecialArgs = commonSpecialArgs;
+        # Use the carefully crafted args for Home Manager modules
+        extraSpecialArgs = homeManagerModuleArgs;
         modules = [
-          ./users/root/hosts/proxmox
-          ./modules/home-manager
+          ./users/root/hosts/proxmox 
+          ./modules/home-manager     
         ];
       };
     };
 
+    # --- Darwin Configurations ---
     darwinConfigurations.macminim4 = inputs.nix-darwin.lib.darwinSystem {
       system = "aarch64-darwin";
-      specialArgs = commonSpecialArgs;
+      specialArgs = systemSpecialArgs; # For Darwin system modules
       modules = [
         {
           nixpkgs.overlays = commonOverlays;
@@ -68,16 +67,18 @@
         }
         ./modules
         ./hosts/macminim4
-
-        inputs.home-manager.darwinModules.home-manager
-        ({ pkgs, ... }: {
+        inputs.home-manager.darwinModules.home-manager # The HM darwin module
+        # Anonymous module to configure HM user and pass correct extraSpecialArgs
+        ({ pkgs, config, lib, inputs, ... }: {
           home-manager.users.deepwatrcreatur = {
             imports = [
               ./users/deepwatrcreatur
               ./users/deepwatrcreatur/hosts/macminim4.nix
-              ./modules/home-manager 
+              ./modules/home-manager
             ];
           };
+          home-manager.extraSpecialArgs = homeManagerModuleArgs;
+
           users.users.deepwatrcreatur = {
             name = "deepwatrcreatur";
             home = "/Users/deepwatrcreatur";
@@ -87,49 +88,34 @@
       ];
     };
 
+    # --- NixOS Configurations ---
     nixosConfigurations = {
       ansible = inputs.nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
-        specialArgs = commonSpecialArgs;
+        specialArgs = systemSpecialArgs; # For NixOS system modules
         modules = [
-          {
-            nixpkgs.overlays = commonOverlays;
-            nixpkgs.config = commonNixpkgsConfig;
-          }
-          ./modules/nix-settings.nix
-          ./hosts/nixos-lxc/ansible
-          ./hosts/nixos
-
-          inputs.sops-nix.nixosModules.sops
+          # ... other NixOS modules ...
           inputs.home-manager.nixosModules.home-manager
-          {
+          # Anonymous module to configure HM user and pass correct extraSpecialArgs
+          ({ config, lib, inputs, ... }: { # These are NixOS module args
             home-manager.users.ansible = {
-              imports = [
-                ./modules
-              ];
+              imports = [ ./modules ];
             };
-          }
+            home-manager.extraSpecialArgs = homeManagerModuleArgs;
+          })
         ];
       };
 
       homeserver = inputs.nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
-        specialArgs = commonSpecialArgs;
+        specialArgs = systemSpecialArgs; # For NixOS system modules
         modules = [
-          {
-            nixpkgs.overlays = commonOverlays;
-            nixpkgs.config = commonNixpkgsConfig;
-          }
           inputs.sops-nix.nixosModules.sops
           inputs.home-manager.nixosModules.home-manager
           ./modules
           ./hosts/nixos
-        ]
-        # This uses the local helper to import modules from a user-defined directory
-        ++ (importAllModulesInDir ./hosts/homeserver/modules)
-        ++ (if builtins.pathExists /etc/nixos/local-secrets.nix
-          then [ /etc/nixos/local-secrets.nix ]
-          else []);
+          (importAllModulesInDir ./hosts/homeserver/modules) 
+        ];
       };
     };
   };
