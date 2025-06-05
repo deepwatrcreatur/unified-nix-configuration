b8812bd1169ef005e247ba372a86c962bc579a65
Anwer Khan <deepwatrcreatur@gmail.com>
2025-06-05 08:19:39 -0400

    using official helix flake to get latest version from github

--- flake-modules/packages.nix
+++ flake-modules/packages.nix
@@ -1,31 +1,144 @@
-# flake-modules/packages.nix
-# This file will return the attribute set for the `packages` output.
-# It needs access to `inputs` (for nixpkgs and lib) and your `overlays`.
-{ inputs, lib, overlaysList, nixpkgsConfig }:
+# flake.nix
+{
+  description = "Multi-system Nix configurations (NixOS, nix-darwin, Home Manager)";
 
-let
-  # Define the systems to generate packages for
-  # can also pass this in as an argument if preferred
-  supportedSystems = [ "x86_64-linux" "aarch64-darwin" ];
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
-  # Helper to generate outputs for all supported systems
-  forAllSystems = function: lib.genAttrs supportedSystems (system: function system);
+    nix-darwin.url = "github:LnL7/nix-darwin/master";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
 
-in
-forAllSystems (system:
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+    sops-nix.url = "github:Mic92/sops-nix";
+    sops-nix.inputs.nixpkgs.follows = "nixpkgs";
+
+    # Helix flake input
+    helix.url = "github:helix-editor/helix";
+    helix.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = inputs@{ ... }:
   let
-    pkgs = import inputs.nixpkgs {
-      inherit system;
-      config = nixpkgsConfig; # apply centralized config 
-      overlays = overlaysList;       # Apply the provided list of overlays
+    # Standard library
+    lib = inputs.nixpkgs.lib;
+
+    commonNixpkgsConfig = {
+      allowUnfree = true;
     };
+
+    commonOverlays = [
+    ];
+
+    # Common specialArgs for NixOS, Darwin, and Home Manager systems
+    # Ensures modules can access flake inputs.
+    commonSpecialArgs = { inherit inputs lib; }; # Pass top-level lib if needed by modules
+
+    # Helper to import all .nix files from a directory as module paths.
+    # This replaces a likely flakeLib.importModules helper.
+    importAllModulesInDir = dir:
+      let
+        items = builtins.readDir dir;
+        # Filter for regular files ending in .nix
+        isNixFile = name: type: type == "regular" && lib.hasSuffix ".nix" name;
+        nixFileNames = lib.attrNames (lib.filterAttrs isNixFile items);
+      in
+        map (fileName: dir + "/${fileName}") nixFileNames;
+
   in
   {
-    # custom helix package
-    helix-from-src = pkgs.helix-from-src;
 
-    default = pkgs.helix-from-src; # Or simply 'self.helix-from-src' to refer to it within the set
+    # --- Home Manager Configurations (Standalone) ---
+    homeConfigurations = {
+      proxmox-root = inputs.home-manager.lib.homeManagerConfiguration {
+        pkgs = import inputs.nixpkgs {
+          system = "x86_64-linux";
+          config = commonNixpkgsConfig;
+          overlays = commonOverlays;
+        };
+        extraSpecialArgs = commonSpecialArgs;
+        modules = [
+          ./users/root/hosts/proxmox
+          ./modules/home-manager 
+        ];
+      };
+    };
 
-    # can add other common packages to expose here
-  }
-)
+    darwinConfigurations.macminim4 = inputs.nix-darwin.lib.darwinSystem {
+      system = "aarch64-darwin";
+      specialArgs = commonSpecialArgs;
+      modules = [
+        {
+          nixpkgs.overlays = commonOverlays;
+          nixpkgs.config = commonNixpkgsConfig;
+        }
+
+        ./modules
+        ./hosts/macminim4
+
+        inputs.home-manager.darwinModules.home-manager
+        ({ pkgs, ... }: {
+          home-manager.users.deepwatrcreatur = {
+            imports = [
+              ./users/deepwatrcreatur
+              ./users/deepwatrcreatur/hosts/macminim4.nix
+              ./modules/home-manager
+            ];
+          };
+          users.users.deepwatrcreatur = {
+            name = "deepwatrcreatur";
+            home = "/Users/deepwatrcreatur";
+            shell = pkgs.fish;
+          };
+        })
+      ];
+    };
+
+    nixosConfigurations = {
+      ansible = inputs.nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs = commonSpecialArgs;
+        modules = [
+          {
+            nixpkgs.overlays = commonOverlays;
+            nixpkgs.config = commonNixpkgsConfig;
+          }
+
+          ./modules/nix-settings.nix
+          ./hosts/nixos-lxc/ansible
+          ./hosts/nixos
+
+          inputs.sops-nix.nixosModules.sops
+          inputs.home-manager.nixosModules.home-manager
+          {
+            home-manager.users.ansible = {
+              imports = [
+                ./modules
+              ];
+            };
+          }
+        ];
+      };
+
+      homeserver = inputs.nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs = commonSpecialArgs;
+        modules = [
+          {
+            nixpkgs.overlays = commonOverlays;
+            nixpkgs.config = commonNixpkgsConfig; # Added for consistency
+          }
+          inputs.sops-nix.nixosModules.sops
+          inputs.home-manager.nixosModules.home-manager
+          ./modules 
+          ./hosts/nixos 
+        ]
+        ++ (importAllModulesInDir ./hosts/homeserver/modules) 
+        ++ (if builtins.pathExists /etc/nixos/local-secrets.nix
+          then [ /etc/nixos/local-secrets.nix ]
+          else []);
+      };
+    };
+  };
+}
