b358729f46f8c4786223280ff8971e10af4fb6b2
Anwer Khan <deepwatrcreatur@gmail.com>
2025-06-08 00:49:45 -0400

    brought homebrew management under nix

---
+++ hosts/macminim4/homebrew.nix
@@ -0,0 +1,22 @@
+# hosts/macminim4/homebrew.nix
+{ config, pkgs, ... }:
+
+{
+  homebrew = {
+    # Formulas (CLI tools)
+    brews = [
+      "aom"
+      "atomicparsley"
+      "cmake"
+      "leptonica"
+      "mise"
+      "msgpack"
+    ];
+
+    # Casks (GUI apps)
+    casks = [
+      "phantomjs"
+      "nightscout-menu-bar"
+    ];
+  };
+}
---
+++ modules/nix-darwin/homebrew.nix
@@ -0,0 +1,30 @@
+# modules/homebrew.nix
+#
+# Contains Homebrew configuration shared across ALL macOS hosts.
+{ config, pkgs, ... }:
+
+{
+  homebrew = {
+    enable = true;
+
+    taps = [
+      "romkatv/powerlevel10k"
+      "homebrew/cask-fonts"
+      "gabe565/tap"
+    ];
+
+    # Formulas i.e. CLI tools
+    brews = [
+      "fish"
+      "cmake"
+      "powerlevel10k"
+    ];
+
+    # Casks (GUI Apps)
+    casks = [
+      "visual-studio-code"
+      "font-fira-code-nerd-font"
+      "font-fira-code"
+    ];
+  };
+}
