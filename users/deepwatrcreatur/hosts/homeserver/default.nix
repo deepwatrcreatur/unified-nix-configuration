152d4be2312b663a3271e6e61540d9ddd128eed2
Anwer Khan <deepwatrcreatur@gmail.com>
2025-06-12 23:34:07 -0400

    added home manger settings for deepwatrcreatur

---
+++ users/deepwatrcreatur/hosts/homeserver/default.nix
@@ -0,0 +1,24 @@
+# users/root/hosts/homeserver/default.nix
+{ config, pkgs, lib, ... }:
+
+{
+  imports = [
+    ../.. # default config for root
+    ./homeserver-justfile.nix
+    ./nh.nix
+  ];
+
+  # Set the username and home directory for Home Manager
+  home.username = "deepwatrcreatur";
+  home.homeDirectory = "/home/deepwatrcreatur"; # Home directory for the root user
+
+  # Add packages
+  home.packages = [
+  ];
+
+  # Configure programs
+  programs.bash.enable = true; 
+  
+  # Let Home Manager manage itself if you want the `home-manager` command available
+  programs.home-manager.enable = true;
+}
