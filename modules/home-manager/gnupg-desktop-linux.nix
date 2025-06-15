5953b0915885e2b2d3ca58080a595cd66b09fd0f
Anwer Khan <deepwatrcreatur@gmail.com>
2025-06-15 01:40:32 -0400

    added missing import of gnupg, corrected a filename

---
+++ modules/home-manager/gnupg-desktop-linux.nix
@@ -0,0 +1,14 @@
+# modules/home-manager/gnupg-desktop-linux.nix
+{ config, pkgs, lib, inputs, ... }:
+
+{
+  imports = [ inputs.home-manager.modules.programs.gnupg ];
+  
+  programs.gnupg = {
+    enable = true;
+    pinentry.enable = true;
+    # Choose one based on your desktop environment (e.g., GNOME, KDE, XFCE)
+    pinentry.program = "${pkgs.pinentry-gtk2}/bin/pinentry-gtk2";
+    # Or pinentry.program = "${pkgs.pinentry-qt}/bin/pinentry-qt";
+  };
+}
