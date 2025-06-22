a3b9675654b723f468d9e41c3d601070a6e9644f
Anwer Khan <deepwatrcreatur@gmail.com>
2025-06-22 07:21:02 -0400

    added missing imports of rclone.nix

--- users/deepwatrcreatur/hosts/macminim4/default.nix
+++ users/deepwatrcreatur/hosts/macminim4/default.nix
@@ -2,6 +2,7 @@
 
 {
   imports = [
+    ../../../../modules/home-manager/rclone.nix
     ../../../../modules/home-manager/ghostty
     ../../../../modules/home-manager/gpg-mac.nix
     ../../../../modules/home-manager/fish-macos.nix
