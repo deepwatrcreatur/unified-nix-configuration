061041548484079f265b24c847b0daa127cf34aa
Anwer Khan <deepwatrcreatur@gmail.com>
2025-06-24 01:36:12 -0400

    more advanced zoxide module that doesn't assume specific user

--- modules/home-manager/zoxide.nix
+++ modules/home-manager/zoxide.nix
@@ -1,13 +1,45 @@
 { config, lib, pkgs, ... }:
 
+with lib;
+
 {
-  home-manager.users.myuser = {
+  # Define options for the module
+  options.programs.zoxide = {
+    enable = mkEnableOption "zoxide, a smarter cd command";
+
+    enableBashIntegration = mkOption {
+      type = types.bool;
+      default = true;
+      description = "Enable zoxide integration for bash.";
+    };
+
+    enableZshIntegration = mkOption {
+      type = types.bool;
+      default = true;
+      description = "Enable zoxide integration for zsh.";
+    };
+
+    enableFishIntegration = mkOption {
+      type = types.bool;
+      default = true;
+      description = "Enable zoxide integration for fish.";
+    };
+
+    enableNushellIntegration = mkOption {
+      type = types.bool;
+      default = true;
+      description = "Enable zoxide integration for nushell.";
+    };
+  };
+
+  # Use Home Manager's built-in zoxide module
+  config = mkIf config.programs.zoxide.enable {
     programs.zoxide = {
       enable = true;
-      enableBashIntegration = true;
-      enableZshIntegration = true;
-      enableFishIntegration = true;
-      enableNushellIntegration = true;
+      enableBashIntegration = config.programs.zoxide.enableBashIntegration;
+      enableZshIntegration = config.programs.zoxide.enableZshIntegration;
+      enableFishIntegration = config.programs.zoxide.enableFishIntegration;
+      enableNushellIntegration = config.programs.zoxide.enableNushellIntegration;
     };
   };
 }
