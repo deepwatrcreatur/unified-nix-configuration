8c18eb4987b2ee06ef2f285314e0380446892d57
Anwer Khan <deepwatrcreatur@gmail.com>
2025-06-25 00:15:11 -0400

    eliminated circular dependency in zoxide module

--- modules/home-manager/zoxide.nix
+++ modules/home-manager/zoxide.nix
@@ -1,16 +1,12 @@
 { config, lib, pkgs, ... }:
 
-with lib;
-
 {
-  # This module doesn't declare new options, it just configures existing ones
-  config = mkIf config.programs.zoxide.enable {
-    programs.zoxide = {
-      enable = true;
-      enableBashIntegration = true;
-      enableZshIntegration = true;
-      enableFishIntegration = true;
-      enableNushellIntegration = true;
-    };
+  # No options needed, just direct configuration
+  programs.zoxide = {
+    enable = true;
+    enableBashIntegration = true;
+    enableZshIntegration = true;
+    enableFishIntegration = true;
+    enableNushellIntegration = true;
   };
 }
