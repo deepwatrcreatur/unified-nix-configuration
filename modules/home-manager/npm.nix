62c759a80800a854d625e14fbd68488e95a673b5
Anwer Khan <deepwatrcreatur@gmail.com>
2025-06-27 03:21:37 -0400

    new approach: manually placing .npmrc using home.file

--- modules/home-manager/npm.nix
+++ modules/home-manager/npm.nix
@@ -1,6 +1,5 @@
 # modules/home-manager/npm.nix
 { config, pkgs, lib, ... }:
-
 {
   options.myModules.npm.enable = lib.mkOption {
     type = lib.types.bool;
@@ -10,22 +9,27 @@
 
   config = lib.mkIf config.myModules.npm.enable {
     home.packages = with pkgs; [
-      nodejs # Essential for npm
+      nodejs # This includes npm
     ];
 
-    programs.nodejs = {
-      enable = true;
-      npm = {
-        npmrc = ''
-          prefix=$HOME/.npm-global
-        '';
-      };
-    };
+    # Create npm configuration file
+    home.file.".npmrc".text = ''
+      prefix=$HOME/.npm-global
+      cache=$HOME/.npm-cache
+      init-author-name=Your Name
+      init-author-email=your.email@example.com
+      init-license=MIT
+    '';
 
-    # Optionally, ensure the global bin directory is in PATH.
-    # Home Manager's programs.npm.enable often handles this, but it's good to be aware.
-    # home.sessionVariables = {
-    #   PATH = [ "$HOME/.npm-global/bin" ];
-    # };
+    # Add npm global bin directory to PATH
+    home.sessionPath = [
+      "$HOME/.npm-global/bin"
+    ];
+
+    # Create the npm global directory if it doesn't exist
+    home.activation.createNpmGlobalDir = lib.hm.dag.entryAfter ["writeBoundary"] ''
+      $DRY_RUN_CMD mkdir -p $HOME/.npm-global
+      $DRY_RUN_CMD mkdir -p $HOME/.npm-cache
+    '';
   };
 }
