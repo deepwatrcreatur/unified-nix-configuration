e2c543b2c1f58b3a7366712f7474bdf7fb134b85
Anwer Khan <deepwatrcreatur@gmail.com>
2025-06-28 08:58:44 -0400

    don't install gemini for root user as sops and gemini credentials not available

--- users/root/default.nix
+++ users/root/default.nix
@@ -4,7 +4,7 @@
     ../../modules/home-manager/git.nix
     ../../modules/home-manager/rename.nix
     ../../modules/home-manager/zoxide.nix
-    ../../modules/home-manager/gemini-cli.nix
+    #../../modules/home-manager/gemini-cli.nix
   ];
   
   home.username = "root";
