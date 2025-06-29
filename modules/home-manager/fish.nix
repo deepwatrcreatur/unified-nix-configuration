f0de8521bf1e3e175b8b09dbf83fcde64423c582
Anwer Khan <deepwatrcreatur@gmail.com>
2025-06-29 08:22:11 -0400

    refactored shell environments, paths, aliases

---
+++ modules/home-manager/fish.nix
@@ -0,0 +1,24 @@
+# modules/home-manager/fish-shared.nix
+
+{ config, pkgs, lib, ... }:
+
+{
+  programs.fish = {
+    enable = true;
+    shellAliases = {
+      rename = "rename -n";
+      rename-apply = "rename";
+    };
+    plugins = [
+      { name = "fzf"; src = pkgs.fishPlugins.fzf; }
+      { name = "z"; src = pkgs.fishPlugins.z; }
+      { name = "puffer"; src = pkgs.fishPlugins.puffer; }
+      { name = "autopair"; src = pkgs.fishPlugins.autopair; }
+      { name = "grc"; src = pkgs.fishPlugins.grc; }
+    ];
+    interactiveShellInit = ''
+      # Set GPG_TTY for all systems
+      set -gx GPG_TTY (tty)
+    '';
+  };
+}
---
+++ modules/home-manager/nushell.nix
@@ -0,0 +1,20 @@
+# modules/home-manager/nushell/default.nix
+
+{ config, pkgs, lib, ... }:
+
+{
+  programs.nushell = {
+    enable = true;
+    environmentVariables = {
+      GPG_TTY = "(tty)";
+    };
+    shellAliases = {
+      rename = "^rename -n";
+      rename-apply = "^rename";
+    };
+  };
+
+  programs.starship = {
+    enableNushellIntegration = true;
+  };
+}
