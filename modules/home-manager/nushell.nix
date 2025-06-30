55e347402320c2ddcaa7d710fdd1e2e597de3bc5
Anwer Khan <deepwatrcreatur@gmail.com>
2025-06-30 09:16:51 -0400

    added bitwarden session secret to nushell environment

--- modules/home-manager/nushell.nix
+++ modules/home-manager/nushell.nix
@@ -7,6 +7,7 @@
     enable = true;
     environmentVariables = {
       GPG_TTY = "(tty)";
+      BW_SESSION = "$(cat ${config.sops.secrets.BW_SESSION.path}";
     };
     shellAliases = {
       rename = "^rename -n";
