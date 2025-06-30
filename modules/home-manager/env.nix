20c5f0245ceab70311460d8a9e2e9514ea174043
Anwer Khan <deepwatrcreatur@gmail.com>
2025-06-30 11:22:14 -0400

    correct path-setting for nushell

--- modules/home-manager/env.nix
+++ modules/home-manager/env.nix
@@ -1,14 +1,11 @@
 # modules/home-manager/env.nix
-
 { config, pkgs, lib, ... }:
-
 {
   home.sessionVariables = {
     EDITOR = "hx";
     VISUAL = "hx";
     GPG_TTY = "(tty)";
   };
-
   home.sessionPath = [
     "${config.home.homeDirectory}/.nix-profile/bin"
     "${config.home.homeDirectory}/.cargo/bin"
@@ -30,13 +27,23 @@
     "/var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/bin"
     "/var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/appleinternal/bin"
   ];
-
+  
+  # Configure zsh to source Home Manager session variables (remove this since you don't use zsh)
+  # programs.zsh = lib.mkIf (pkgs.stdenv.isDarwin) {
+  #   enable = true;
+  #   initExtra = ''
+  #     # Source Home Manager session variables
+  #     if [ -f ~/.nix-profile/etc/profile.d/hm-session-vars.sh ]; then
+  #       source ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+  #     fi
+  #   '';
+  # };
+  
   home.file = lib.mkIf (pkgs.stdenv.isDarwin) {
     "Library/Application Support/nushell/config.nu".source = pkgs.writeTextFile {
       name = "nushell-config";
       text = ''
         $env.GNUPGHOME = "${config.home.homeDirectory}/.gnupg"
-
         if ($env.SSH_AUTH_SOCK | is-empty) and ("/opt/homebrew/bin/gpgconf" | path exists) {
           $env.SSH_AUTH_SOCK = (^/opt/homebrew/bin/gpgconf --list-dirs agent-ssh-socket | str trim)
         } else if ($env.SSH_AUTH_SOCK | is-empty) and ("/run/current-system/sw/bin/gpgconf" | path exists) {
@@ -49,16 +56,36 @@
       text = "";
     };
   };
-
   programs.nushell = {
     enable = true;
+    envFile.text = lib.mkIf (pkgs.stdenv.isDarwin) ''
+      # Set up PATH with all required directories
+      $env.PATH = ($env.PATH | split row (char esep) | prepend [
+        "${config.home.homeDirectory}/.nix-profile/bin"
+        "${config.home.homeDirectory}/.cargo/bin" 
+        "${config.home.homeDirectory}/.npm-global/bin"
+        "/opt/homebrew/bin"
+        "/opt/homebrew/opt/mise/bin"
+        "/usr/bin"
+        "/bin" 
+        "/usr/sbin"
+        "/sbin"
+        "/usr/local/bin"
+        "/run/current-system/sw/bin"
+        "/nix/var/nix/profiles/default/bin"
+        "/usr/local/MacGPG2/bin"
+        "/Applications/Ghostty.app/Contents/MacOS"
+        "/System/Cryptexes/App/usr/bin"
+        "/var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/local/bin"
+        "/var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/bin"
+        "/var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/appleinternal/bin"
+      ] | uniq)
+    '';
   };
-
   programs.fish = {
     enable = true;
     interactiveShellInit = lib.mkIf (pkgs.stdenv.isDarwin) ''
       set -gx GNUPGHOME ${config.home.homeDirectory}/.gnupg
-
       if test -z "$SSH_AUTH_SOCK" -a -x /opt/homebrew/bin/gpgconf
         set -gx SSH_AUTH_SOCK (/opt/homebrew/bin/gpgconf --list-dirs agent-ssh-socket | str trim)
       else if test -z "$SSH_AUTH_SOCK" -a -x /run/current-system/sw/bin/gpgconf
