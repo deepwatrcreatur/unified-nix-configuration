6cecb60a2a7941fa8c49485c088daea82ed1ef43
Anwer Khan <deepwatrcreatur@gmail.com>
2025-07-01 00:52:19 -0400

    split environment ocnfiguration module into two, including darwin-specific

---
+++ modules/home-manager/env-darwin.nix
@@ -0,0 +1,72 @@
+{ config, pkgs, lib, ... }:
+{
+  home.sessionPath = [
+    "/opt/homebrew/bin"
+    "/opt/homebrew/opt/mise/bin"
+    "/usr/bin"
+    "/bin"
+    "/usr/sbin"
+    "/sbin"
+    "/usr/local/bin"
+    "/run/current-system/sw/bin"
+    "/nix/var/nix/profiles/default/bin"
+    "/usr/local/MacGPG2/bin"
+    "/Applications/Ghostty.app/Contents/MacOS"
+    "/System/Cryptexes/App/usr/bin"
+    "/var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/local/bin"
+    "/var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/bin"
+    "/var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/appleinternal/bin"
+  ];
+  home.file = {
+    "Library/Application Support/nushell/config.nu".source = pkgs.writeTextFile {
+      name = "nushell-config";
+      text = ''
+        $env.GNUPGHOME = "${config.home.homeDirectory}/.gnupg"
+        if ($env.SSH_AUTH_SOCK | is-empty) and ("/opt/homebrew/bin/gpgconf" | path exists) {
+          $env.SSH_AUTH_SOCK = (^/opt/homebrew/bin/gpgconf --list-dirs agent-ssh-socket | str trim)
+        } else if ($env.SSH_AUTH_SOCK | is-empty) and ("/run/current-system/sw/bin/gpgconf" | path exists) {
+          $env.SSH_AUTH_SOCK = (^/run/current-system/sw/bin/gpgconf --list-dirs agent-ssh-socket | str trim)
+        }
+      '';
+    };
+    "Library/Application Support/nushell/env.nu".source = pkgs.writeTextFile {
+      name = "nushell-env";
+      text = "";
+    };
+  };
+  programs.nushell = {
+    envFile.text = ''
+      # Set up PATH with all required directories
+      $env.PATH = ($env.PATH | split row (char esep) | prepend [
+        "${config.home.homeDirectory}/.nix-profile/bin"
+        "${config.home.homeDirectory}/.cargo/bin"
+        "${config.home.homeDirectory}/.npm-global/bin"
+        "/opt/homebrew/bin"
+        "/opt/homebrew/opt/mise/bin"
+        "/usr/bin"
+        "/bin"
+        "/usr/sbin"
+        "/sbin"
+        "/usr/local/bin"
+        "/run/current-system/sw/bin"
+        "/nix/var/nix/profiles/default/bin"
+        "/usr/local/MacGPG2/bin"
+        "/Applications/Ghostty.app/Contents/MacOS"
+        "/System/Cryptexes/App/usr/bin"
+        "/var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/local/bin"
+        "/var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/bin"
+        "/var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/appleinternal/bin"
+      ] | uniq)
+    '';
+  };
+  programs.fish = {
+    interactiveShellInit = ''
+      set -gx GNUPGHOME ${config.home.homeDirectory}/.gnupg
+      if test -z "$SSH_AUTH_SOCK" -a -x /opt/homebrew/bin/gpgconf
+        set -gx SSH_AUTH_SOCK (/opt/homebrew/bin/gpgconf --list-dirs agent-ssh-socket | str trim)
+      else if test -z "$SSH_AUTH_SOCK" -a -x /run/current-system/sw/bin/gpgconf
+        set -gx SSH_AUTH_SOCK (/run/current-system/sw/bin/gpgconf --list-dirs agent-ssh-socket | str trim)
+      end
+    '';
+  };
+}
--- users/deepwatrcreatur/hosts/macminim4/default.nix
+++ users/deepwatrcreatur/hosts/macminim4/default.nix
@@ -5,6 +5,8 @@
     ../../../../modules/home-manager/rclone.nix
     ../../../../modules/home-manager/ghostty
     ../../../../modules/home-manager/gpg-mac.nix
+    ../../../../modules/home-manager/env-darwin.nix
+    ../../../../modules/home-manager/env.nix
     ../../xbar.nix
   ];
 
