9993d3ee26179e746557703e90760bd4623cd54a
Anwer Khan <deepwatrcreatur@gmail.com>
2025-07-01 01:46:13 -0400

    moved more packages out of homeserver-specific config into home manager default.nix

--- hosts/homeserver/modules/packages.nix
+++ hosts/homeserver/modules/packages.nix
@@ -2,28 +2,9 @@
 
 {
   environment.systemPackages = with pkgs; [
-    nh
-    nushell
-    fzf
-    yazi
-    htop
-    btop
-    gitAndTools.gh
-    wget
-    curl
-    iperf3
-    age
-    oh-my-posh
-    tmux
-    nmap
-    bat
     nix-inspect
     compose2nix
     docker
-    nodejs_20
-    rsync
-    elixir
-    erlang
     docker-compose
   ];
 }
