45cfd6dcd86882d0480ae62bcf5864ed917636d5
Anwer Khan <deepwatrcreatur@gmail.com>
2025-07-02 16:16:11 -0400

    re-enabling rclone and placing rclone.conf in xdg home

--- users/deepwatrcreatur/sops.nix
+++ users/deepwatrcreatur/sops.nix
@@ -42,10 +42,9 @@ in
         sopsFile = "${sopsSecretsDir}/bitwarden.yaml";
         format = "yaml";
       };
-
       secrets."bitwarden_data_json" = {
         sopsFile = "${sopsSecretsDir}/data.json.enc";
-        format = "json";
+        format = "binary";
         path = "${config.xdg.configHome}/Bitwarden CLI/data.json";
         mode = "0600";
       };
