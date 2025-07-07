37da2108d136b841e7adbc1b7c8b727f8fdc39aa
Anwer Khan <deepwatrcreatur@gmail.com>
2025-07-07 00:40:54 -0400

    added module enabling zed editor

--- users/deepwatrcreatur/hosts/macminim4/default.nix
+++ users/deepwatrcreatur/hosts/macminim4/default.nix
@@ -4,6 +4,7 @@
   imports = [
     ../../../../modules/home-manager/rclone.nix
     ../../../../modules/home-manager/ghostty
+    ../../../../modules/home-manager/zed.nix
     ../../../../modules/home-manager/gpg-mac.nix
     ../../../../modules/home-manager/env-darwin.nix
     ../../../../modules/home-manager/env.nix
