ab665ce17c25ef97787af084c3784a0d61e29da8
Anwer Khan <deepwatrcreatur@gmail.com>
2025-07-08 01:20:27 -0400

    simplified atuin module to use built-in package from home manager

--- modules/home-manager/atuin.nix
+++ modules/home-manager/atuin.nix
@@ -1,56 +1,18 @@
 { config, pkgs, lib, ... }:
 
-let
-  cfg = config.programs.atuin;
-in
 {
-  options.programs.atuin = {
-    enable = lib.mkEnableOption "Atuin, a modern shell history replacement.";
+  programs.atuin.enable = true;
 
-    package = lib.mkPackageOption pkgs "atuin" {};
-
-    description = "The Atuin package to use.";
-    # extraConfig = lib.mkOption {
-    #   type = lib.types.lines;
-    #   default = "";
-    #   description = "Additional lines for Atuin config file.";
-    # };
-  };
-
-  config = lib.mkIf cfg.enable {
-    # Ensure the Atuin package is available
-    home.packages = [ cfg.package ];
-
-    # Configure the shell to use Atuin
-    programs.bash = {
-      initExtra = ''
-        eval "$(${cfg.package}/bin/atuin init bash)"
-      '';
-    };
-
-    programs.zsh = {
-      initExtra = ''
-        eval "$(${cfg.package}/bin/atuin init zsh)"
-      '';
-    };
-
-    programs.fish = {
-      shellInit = lib.mkAfter ''
-        atuin init fish | source
-      '';
-    };
-
-    programs.nushell = {
-      extraConfig = ''
-        atuin init nu | source
-      '';
-    };
-
-    # xdg.configFile."atuin/config.toml".source =
-    #   lib.mkIf (cfg.sync) (pkgs.writeText "atuin-config.toml" ''
-    #     sync_frequency = "10m"
-    #     auto_sync = true
-    #     ${cfg.extraConfig}
-    #   '');
-  };
+  programs.bash.initExtra = ''
+    eval "$(${pkgs.atuin}/bin/atuin init bash)"
+  '';
+  programs.zsh.initExtra = ''
+    eval "$(${pkgs.atuin}/bin/atuin init zsh)"
+  '';
+  programs.fish.shellInit = lib.mkAfter ''
+    atuin init fish | source
+  '';
+  programs.nushell.extraConfig = ''
+    atuin init nu | source
+  ''; 
 }
