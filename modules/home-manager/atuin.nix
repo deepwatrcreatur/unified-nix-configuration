b96aaf5a2130e4748b558b2904a49eb79686ba8d
Anwer Khan <deepwatrcreatur@gmail.com>
2025-07-11 00:58:48 -0400

    removed custom shell init sections from atuin module to test if they are really necessary

--- modules/home-manager/atuin.nix
+++ modules/home-manager/atuin.nix
@@ -2,17 +2,4 @@
 
 {
   programs.atuin.enable = true;
-
-  programs.bash.initExtra = ''
-    eval "$(${pkgs.atuin}/bin/atuin init bash)"
-  '';
-  programs.zsh.initExtra = ''
-    eval "$(${pkgs.atuin}/bin/atuin init zsh)"
-  '';
-  programs.fish.shellInit = lib.mkAfter ''
-    atuin init fish | source
-  '';
-
-  programs.nushell.extraConfig = ''
-  '';  
 }
