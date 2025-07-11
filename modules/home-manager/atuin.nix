8df2e7f9c4e0b5d2699d43b66b062bef444a933e
Anwer Khan <deepwatrcreatur@gmail.com>
2025-07-11 00:42:48 -0400

    create placeholder file for nushell atuin init then update

--- modules/home-manager/atuin.nix
+++ modules/home-manager/atuin.nix
@@ -17,8 +17,20 @@
   home.file.".config/nushell/atuin-init.nu".source = pkgs.runCommand "atuin-nushell-init" {} ''
     ${pkgs.atuin}/bin/atuin init nu > $out
   '';
+
+  # Create placeholder file
+  home.file.".config/nushell/atuin.nu".text = ''
+    # Atuin integration for nushell
+    # This file will be populated when you first run nushell
+  '';
   
   programs.nushell.extraConfig = ''
-    source ~/.config/nushell/atuin-init.nu
-  '';
+    # Initialize atuin if not already done
+    let atuin_file = ($env.HOME | path join ".config" "nushell" "atuin.nu")
+    let atuin_content = (open $atuin_file)
+    if ($atuin_content | str contains "# This file will be populated") {
+      atuin init nu | save --force $atuin_file
+    }
+    source $atuin_file
+  '';  
 }
