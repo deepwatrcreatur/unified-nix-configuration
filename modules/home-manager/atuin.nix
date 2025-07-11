a648c509c7283db442d39d169b373bb95959023c
Anwer Khan <deepwatrcreatur@gmail.com>
2025-07-11 00:38:32 -0400

    create nushell atuin init in temp file then load

--- modules/home-manager/atuin.nix
+++ modules/home-manager/atuin.nix
@@ -12,7 +12,13 @@
   programs.fish.shellInit = lib.mkAfter ''
     atuin init fish | source
   '';
+
+  # Generate atuin config file during build
+  home.file.".config/nushell/atuin-init.nu".source = pkgs.runCommand "atuin-nushell-init" {} ''
+    ${pkgs.atuin}/bin/atuin init nu > $out
+  '';
+  
   programs.nushell.extraConfig = ''
-    atuin init nu | nu --stdin
-  ''; 
+    source ~/.config/nushell/atuin-init.nu
+  '';
 }
