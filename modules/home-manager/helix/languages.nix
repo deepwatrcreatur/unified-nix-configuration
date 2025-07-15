1970cee0316f574b286bccbf8a62e7f2fe12a095
Anwer Khan <deepwatrcreatur@gmail.com>
2025-07-15 01:17:05 -0400

    added config to helix: more languages and formats

--- modules/home-manager/helix/languages.nix
+++ modules/home-manager/helix/languages.nix
@@ -1,4 +1,5 @@
-{ pkgs }: # Accept pkgs as an argument
+{ pkgs }:
+
 {
   language = [
     {
@@ -11,22 +12,96 @@
       name = "elixir";
       auto-format = true;
       language-servers = [ "elixir-ls" ];
-      # Add formatter if needed for Elixir
-      # formatter = { command = "mix"; args = ["format", "-"]; };
     }
-    # Add other languages here if needed
+    {
+      name = "yaml";
+      auto-format = true;
+      language-servers = [ "yaml-language-server" ];
+      formatter = { command = "${pkgs.yaml-language-server}/bin/yamlfmt"; args = [ "-in" ]; };
+    }
+    {
+      name = "python";
+      auto-format = true;
+      language-servers = [ "pyright" ];
+      formatter = { command = "${pkgs.black}/bin/black"; args = [ "--quiet" "-" ]; };
+    }
+    {
+      name = "javascript";
+      auto-format = true;
+      language-servers = [ "typescript-language-server" ];
+      formatter = { command = "${pkgs.prettier}/bin/prettier"; args = [ "--parser" "javascript" ]; };
+    }
+    {
+      name = "typescript";
+      auto-format = true;
+      language-servers = [ "typescript-language-server" ];
+      formatter = { command = "${pkgs.prettier}/bin/prettier"; args = [ "--parser" "typescript" ]; };
+    }
+    {
+      name = "go";
+      auto-format = true;
+      language-servers = [ "gopls" ];
+      formatter = { command = "${pkgs.go}/bin/gofmt"; args = [ "-s" ]; };
+    }
   ];
   language-server = {
     nil = {
-      command = "${pkgs.nil}/bin/nil"; # Explicit path is good practice
+      command = "${pkgs.nil}/bin/nil";
       config.nil = { formatting.command = [ "${pkgs.nixpkgs-fmt}/bin/nixpkgs-fmt" ]; };
     };
 
     nixd = { command = "${pkgs.nixd}/bin/nixd"; };
 
     "elixir-ls" = {
-      command = "${pkgs.elixir-ls}/bin/language_server.sh"; # Use the wrapper script
+      command = "${pkgs.elixir-ls}/bin/language_server.sh";
+    };
+
+    "yaml-language-server" = {
+      command = "${pkgs.yaml-language-server}/bin/yaml-language-server";
+      args = [ "--stdio" ];
+      config.yaml = {
+        format.enable = true;
+        validation = true;
+        keyOrdering = false;
+        schemaStore.enable = true;
+        schemas = {
+          "https://gitlab.com/gitlab-org/gitlab/-/raw/master/app/assets/javascripts/editor/schema/ci.json" = "*gitlab-ci*.{yml,yaml}";
+          "https://json.schemastore.org/ansible-playbook" = "*play*.{yml,yaml}";
+          "https://json.schemastore.org/ansible-stable-2.9" = "roles/tasks/*.{yml,yaml}";
+          "https://json.schemastore.org/chart" = "Chart.{yml,yaml}";
+          "https://json.schemastore.org/dependabot-v2" = ".github/dependabot.{yml,yaml}";
+          "https://json.schemastore.org/github-action" = ".github/action.{yml,yaml}";
+          "https://json.schemastore.org/github-workflow" = ".github/workflows/*";
+          "https://json.schemastore.org/prettierrc" = ".prettierrc.{yml,yaml}";
+          "https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/schemas/v3.1/schema.json" = "*api*.{yml,yaml}";
+          "https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json" = "*docker-compose*.{yml,yaml}";
+          kubernetes = "*.y{a,}ml";
+        };
+      };
+    };
+
+    pyright = {
+      command = "${pkgs.pyright}/bin/pyright-langserver";
+      args = [ "--stdio" ];
+      config = {
+        python.analysis.typeCheckingMode = "basic";
+      };
+    };
+
+    "typescript-language-server" = {
+      command = "${pkgs.typescript-language-server}/bin/typescript-language-server";
+      args = [ "--stdio" ];
+    };
+
+    gopls = {
+      command = "${pkgs.gopls}/bin/gopls";
+      config = {
+        "formatting.gofumpt" = true;
+        "ui.inlayhint.hints" = {
+          parameterHints = true;
+          typeHints = true;
+        };
+      };
     };
   };
 }
-
