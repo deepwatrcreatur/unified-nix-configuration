be3ca4a9caebed28421fbb0e89e3382338a38a3f
Anwer Khan <deepwatrcreatur@gmail.com>
2025-07-15 01:17:05 -0400

    added config to helix: more languages and formats

--- modules/home-manager/helix/default.nix
+++ modules/home-manager/helix/default.nix
@@ -1,18 +1,22 @@
-
-# modules/home-manager/helix-config.nix
-{ config, pkgs, lib, inputs, ... }: # Ensure 'inputs' is here
+{ config, pkgs, lib, inputs, ... }:
 
 {
   programs.helix = {
     enable = true;
-    # Use the package from the Helix flake input
     package = inputs.helix.packages.${pkgs.system}.helix;
-    defaultEditor = true; 
+    defaultEditor = true;
     extraPackages = with pkgs; [
       nil
       nixd
       nixpkgs-fmt
       elixir-ls
+      yaml-language-server
+      pyright
+      black
+      typescript-language-server
+      prettier
+      gopls
+      go
     ];
     settings = import ./settings.nix;
     languages = import ./languages.nix { inherit pkgs; };
