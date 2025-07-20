e69844b4e39c7f2664fae630f7f1e94209c0fb8c
Anwer Khan <deepwatrcreatur@gmail.com>
2025-07-20 00:20:44 -0400

    created new host: nixos_lxc

---
+++ hosts/nixos_lxc/modules/configuration.nix
@@ -0,0 +1,26 @@
+{ config, lib, modulesPath, pkgs, ... }:
+
+{
+  imports = [
+    "${modulesPath}/virtualisation/lxc-container.nix"
+    ../../../modules/nixos/networking.nix
+    ../../../modules/nixos
+    ../../../modules/nixos/services/iperf3.nix
+  ];
+
+  networking.hostName = "nixos_lxc";
+
+  services.nginx-proxy-manager.enable = true;  
+
+  security.sudo.wheelNeedsPassword = false;
+
+  systemd.mounts = [{
+    what = "debugfs";
+    where = "/sys/kernel/debug";
+    enable = false;
+  }];
+
+  boot.initrd.systemd.fido2.enable = false;
+
+  system.stateVersion = "25.05";
+}
