9754506a84d354586a0c53182f45527b629c01a3
Anwer Khan <deepwatrcreatur@gmail.com>
2025-08-03 05:36:19 -0400

    special handling of aliases in nushell

--- modules/home-manager/shell-aliases.nix
+++ modules/home-manager/shell-aliases.nix
@@ -1,9 +1,7 @@
 # modules/home-manager/shell-aliases.nix
-
 { config, pkgs, lib, ... }:
-
-{
-  home.shellAliases = {
+let
+  aliases = {
     ls = "lsd";
     ll = "lsd -l";
     la = "lsd -a";
@@ -14,4 +12,21 @@
   } // lib.optionalAttrs (pkgs.stdenv.isDarwin) {
     xcode = "open -a Xcode";
   };
+in
+{
+  programs.bash.shellAliases = aliases;
+  programs.zsh.shellAliases = aliases;
+  programs.fish.shellAliases = aliases;
+  
+  # Handle nushell separately with proper syntax
+  programs.nushell.extraConfig = ''
+    alias ls = ^lsd
+    alias ll = ^lsd -l
+    alias la = ^lsd -a
+    alias lla = ^lsd -la
+    alias ".." = cd ..
+    alias update = ^just --justfile ~/.justfile update
+    alias nh-update = ^just --justfile ~/.justfile nh-update
+    ${lib.optionalString (pkgs.stdenv.isDarwin) "alias xcode = ^open -a Xcode"}
+  '';
 }
