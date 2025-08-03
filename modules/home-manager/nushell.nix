7ba869de0813e587284cca62e22352e84ff6eb93
Anwer Khan <deepwatrcreatur@gmail.com>
2025-08-03 05:05:14 -0400

    moved lots of nushell config out of env-darwin.nix into nushell.nix

--- modules/home-manager/nushell.nix
+++ modules/home-manager/nushell.nix
@@ -1,16 +1,56 @@
 # modules/home-manager/nushell/default.nix
-
 { config, pkgs, lib, ... }:
-
 {
   programs.nushell = {
     enable = true;
+    
     environmentVariables = {
       GPG_TTY = "(tty)";
+      GNUPGHOME = "${config.home.homeDirectory}/.gnupg";
+      SOPS_AGE_KEY_FILE = "${config.home.homeDirectory}/.config/sops/age/keys.txt";
     };
+    
     shellAliases = {
       rename = "^rename -n";
       rename-apply = "^rename";
+      # Add other aliases here if you want them in nushell
     };
+    
+    configFile.text = ''
+      # SSH auth socket setup
+      if ($env.SSH_AUTH_SOCK | is-empty) and ("/opt/homebrew/bin/gpgconf" | path exists) {
+        $env.SSH_AUTH_SOCK = (^/opt/homebrew/bin/gpgconf --list-dirs agent-ssh-socket | str trim)
+      } else if ($env.SSH_AUTH_SOCK | is-empty) and ("/run/current-system/sw/bin/gpgconf" | path exists) {
+        $env.SSH_AUTH_SOCK = (^/run/current-system/sw/bin/gpgconf --list-dirs agent-ssh-socket | str trim)
+      }
+      
+      # Starship prompt integration
+      $env.STARSHIP_SHELL = "nu"
+      $env.PROMPT_COMMAND = { ||
+          ^/usr/local/bin/starship prompt --cmd-duration $env.CMD_DURATION_MS $"--status=($env.LAST_EXIT_CODE)"
+      }
+      $env.PROMPT_COMMAND_RIGHT = { ||
+          ^/usr/local/bin/starship prompt --right --cmd-duration $env.CMD_DURATION_MS $"--status=($env.LAST_EXIT_CODE)"
+      }
+    '';
+    
+    envFile.text = ''
+      # Set up PATH with all required directories
+      $env.PATH = ($env.PATH | split row (char esep) | prepend [
+        "${config.home.homeDirectory}/.nix-profile/bin"
+        "/opt/homebrew/bin"
+        "/opt/homebrew/opt/mise/bin"
+        "/usr/bin"
+        "/bin"
+        "/usr/sbin"
+        "/sbin"
+        "/usr/local/bin"
+        "/run/current-system/sw/bin"
+        "/nix/var/nix/profiles/default/bin"
+        "/usr/local/MacGPG2/bin"
+        "/Applications/Ghostty.app/Contents/MacOS"
+        "/System/Cryptexes/App/usr/bin"
+      ] | uniq)
+    '';
   };
 }
