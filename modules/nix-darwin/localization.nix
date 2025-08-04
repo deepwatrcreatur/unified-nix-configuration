a018380c9bd6c8b3ffd7270f8daa9b8a90edd66b
Anwer Khan <deepwatrcreatur@gmail.com>
2025-08-04 02:44:58 -0400

    added some more localization settingsc:

--- modules/nix-darwin/localization.nix
+++ modules/nix-darwin/localization.nix
@@ -1,9 +1,23 @@
 {
   system.defaults.NSGlobalDomain = {
-    AppleICUForce24HourTime = false;
-
+    # Time and Date
+    AppleICUForce24HourTime = true;
+    
+    # Units
     AppleMeasurementUnits = "Centimeters";
     AppleMetricUnits      = 1;
     AppleTemperatureUnit  = "Celsius";
+    
+    # Language and Region
+    AppleLanguages = [ "en-CA" ];  # Canadian English preference
+    AppleLocale = "en_CA@currency=CAD";  # Canadian locale with CAD currency
+    
+    # Number and Currency Formatting
+    AppleICUNumberSymbols = {
+      "0"  = 46;   # Decimal separator (period)
+      "1"  = 44;   # Thousands separator (comma)
+      "10" = 46;  # Monetary decimal separator
+      "17" = 36;  # Currency symbol ($) - CAD uses same symbol  };
+    };
   };
 }
