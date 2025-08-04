e145ca85490bbe8edf2a644be9d3e5584deeb132
Anwer Khan <deepwatrcreatur@gmail.com>
2025-08-04 02:47:02 -0400

    removed invalid localization option

--- modules/nix-darwin/localization.nix
+++ modules/nix-darwin/localization.nix
@@ -11,13 +11,5 @@
     # Language and Region
     AppleLanguages = [ "en-CA" ];  # Canadian English preference
     AppleLocale = "en_CA@currency=CAD";  # Canadian locale with CAD currency
-    
-    # Number and Currency Formatting
-    AppleICUNumberSymbols = {
-      "0"  = 46;   # Decimal separator (period)
-      "1"  = 44;   # Thousands separator (comma)
-      "10" = 46;  # Monetary decimal separator
-      "17" = 36;  # Currency symbol ($) - CAD uses same symbol  };
-    };
   };
 }
