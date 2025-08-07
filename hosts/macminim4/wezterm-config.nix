3ee0e98b9dc61dbbb5f3f8361272cdb66990ad9e
Anwer Khan <deepwatrcreatur@gmail.com>
2025-08-07 14:32:11 -0400

    added wezterm module for macOS and Linux hosts, and config module that activates for macminim4

---
+++ hosts/macminim4/wezterm-config.nix
@@ -0,0 +1,28 @@
+{
+  imports = [ ../../modules/wezterm.nix ];
+  
+  programs.wezterm = {
+    enable = true;
+    
+    font = {
+      name = "JetBrains Mono";
+      size = 13.0;
+    };
+    
+    # colorScheme = "Tokyo Night";
+    
+    window.opacity = 0.95;
+    
+    macos.nativeFullscreen = true;  # Only applied on macOS
+    #linux.enableWayland = true;    # Only applied on Linux
+    
+    keyBindings = [
+      { key = "t"; mods = "CTRL|SHIFT"; action = "SpawnTab 'CurrentPaneDomain'"; }
+    ];
+    
+    extraConfig = ''
+      -- Any additional Lua configuration
+      config.scrollback_lines = 5000
+    '';
+  };
+}
