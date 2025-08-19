8113a074f70ae5f34b07b59ffba44062cc7e5a2b
Anwer Khan <deepwatrcreatur@gmail.com>
2025-08-19 12:51:48 -0400

    created auto-import directory for common home-manager modules, adjusted default module to load from there

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -1,53 +1,20 @@
 # modules/home-manager/default.nix
 { config, pkgs, lib, ... }:
+let
+  # Helper to import all .nix files from common directory
+  commonDir = ./common;
+  commonFiles = builtins.readDir commonDir;
+  commonModules = lib.filterAttrs (name: type: type == "regular" && lib.hasSuffix ".nix" name) commonFiles;
+  commonImports = lib.mapAttrsToList (name: _: commonDir + "/${name}") commonModules;
+in
 {
-  imports = [
-    ./env.nix # shell environment settings
-    ./nushell.nix
-    ./helix
-    ./fish.nix
-    ./fd-enhanced.nix
-    ./ripgrep-enhanced.nix
-    ./jujutsu
-    ./starship.nix
-    ./cargo-binstall.nix
-    ./yazelix.nix
-    ./npm.nix
-    ./eza.nix 
-    ./fzf.nix
-    ./shell-aliases.nix
-    ./sshs.nix
-   ];
-
-  home.packages = with pkgs; [
-    comma
-    wget
-    curl
-    xh
-    fastfetch
-    nmap
-    htop
-    btop
-    iperf3
-    yamllint
-    file
-    lsd
-    bat
-    tmux
-    neovim
-    flow-control
-    python3
-    glow
-    age
-  ];
-
+  imports = commonImports;
+  
   # Copy .terminfo files into place
   home.file.".terminfo" = {
     source = ../../modules/home-manager/terminfo;
     recursive = true;
   };
-
   home.file.".ssh/config".source = ./ssh-config;
-
   programs.home-manager.enable = true;
 }
