4e6a7f165c4a0816a0f88648beffa0ff8f33f5c1
Anwer Khan <deepwatrcreatur@gmail.com>
2025-08-19 13:11:40 -0400

    removed obsolete import now that common home manager modules organized in dedicated auto-import directory, moved some packages into common packages module

--- users/deepwatrcreatur/hosts/macminim4/default.nix
+++ users/deepwatrcreatur/hosts/macminim4/default.nix
@@ -7,7 +7,6 @@
     ../../../../modules/home-manager/zed.nix
     ../../../../modules/home-manager/gpg-mac.nix
     ../../../../modules/home-manager/env-darwin.nix
-    ../../../../modules/home-manager/env.nix
     ../../xbar.nix
     ../../rbw.nix
   ];
@@ -15,9 +14,6 @@
   home.packages = with pkgs; [
     cot 
     bitwarden
-    jq
-    yq
-    nh
     megacmd
     ffmpeg
     yt-dlp
