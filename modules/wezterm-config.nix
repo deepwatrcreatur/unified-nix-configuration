9fc98fe166c894c81308e6af5dd2f12a71965085
Anwer Khan <deepwatrcreatur@gmail.com>
2025-08-22 01:22:26 -0400

    corrected bad copy/paste that duplicated module content

--- modules/wezterm-config.nix
+++ modules/wezterm-config.nix
@@ -35,42 +35,4 @@
       }
     '';
   };
-}# modules/wezterm-config.nix - Your configuration file
-{
-  imports = [ ./wezterm.nix ];
-  programs.wezterm = {
-    enable = true;
-    font = {
-      name = "JetBrains Mono";
-      size = 16.0;
-    };
-    colorScheme = "Catppuccin Mocha";
-    window = {
-      opacity = 1.0;
-      decorations = "RESIZE";
-      adjustSizeWhenChangingFont = false;
-    };
-    tabs.enable = false;
-    scrollbackLines = 5000;
-    macos = {
-      nativeFullscreen = true;
-      windowBackgroundBlur = 30;
-    };
-    linux.enableWayland = true;
-    keyBindings = [
-      { key = "q"; mods = "CTRL"; action = "ToggleFullScreen"; }
-      { key = "'"; mods = "CTRL"; action = "ClearScrollback 'ScrollbackAndViewport'"; }
-    ];
-    mouseBindings = [
-      # Temporarily commented out to debug
-      # {
-      #   event = "Up = { streak = 1, button = \"Left\" }";
-      #   mods = "CTRL";
-      #   action = "OpenLinkAtMouseCursor";
-      # }
-    ];
-    extraConfig = ''
-      -- Any additional custom Lua configuration
-    '';
-  };
 }
