b87920e69517b0c12e629698abc498d928ef08f3
Anwer Khan <deepwatrcreatur@gmail.com>
2025-08-25 06:55:04 -0400

    added module for bat CLI command

---
+++ modules/home-manager/common/bat.nix
@@ -0,0 +1,21 @@
+{ config, lib, pkgs, ... }: let
+  inherit (lib) enabled;
+in {
+  environment.variables = {
+    MANPAGER = "bat --plain";
+    PAGER    = "bat --plain";
+  };
+  environment.shellAliases = {
+    cat  = "bat";
+    less = "bat --plain";
+  };
+
+  home-manager.sharedModules = [{
+    programs.bat = enabled {
+      config.theme      = "onehalf";
+      themes.onehalf.src = pkgs.writeText "onehalf.tmTheme" config.theme.tmTheme;
+
+      config.pager = "less --quit-if-one-screen --RAW-CONTROL-CHARS";
+    };
+  }];
+}
