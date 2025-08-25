febc067bfd4eaebb092185aaa7e91492900fc780
Anwer Khan <deepwatrcreatur@gmail.com>
2025-08-25 07:00:24 -0400

    corrected form to work in home manager

--- modules/home-manager/common/bat.nix
+++ modules/home-manager/common/bat.nix
@@ -1,31 +1,19 @@
 { config, lib, pkgs, ... }: let
   inherit (lib) enabled;
 in {
-  environment.variables = {
+  programs.bat = enabled {
+    config.theme      = "onehalf";
+    themes.onehalf.src = pkgs.writeText "onehalf.tmTheme" config.theme.tmTheme;
+    config.pager = "less --quit-if-one-screen --RAW-CONTROL-CHARS";
+  };
+  
+  home.sessionVariables = {
     MANPAGER = "bat --plain";
     PAGER    = "bat --plain";
   };
   
-  environment.shellAliases = {
+  home.shellAliases = {
     cat  = "bat";
     less = "bat --plain";
   };
-  
-  home-manager.sharedModules = [{
-    programs.bat = enabled {
-      config.theme      = "onehalf";
-      themes.onehalf.src = pkgs.writeText "onehalf.tmTheme" config.theme.tmTheme;
-      config.pager = "less --quit-if-one-screen --RAW-CONTROL-CHARS";
-    };
-    
-    home.sessionVariables = {
-      MANPAGER = "bat --plain";
-      PAGER    = "bat --plain";
-    };
-    
-    home.shellAliases = {
-      cat  = "bat";
-      less = "bat --plain";
-    };
-  }];
 }
