f9c4839268488b06214d127088ab127580806b60
Anwer Khan <deepwatrcreatur@gmail.com>
2025-08-25 06:56:46 -0400

    converted system module to home manager

--- modules/home-manager/common/bat.nix
+++ modules/home-manager/common/bat.nix
@@ -5,17 +5,18 @@ in {
     MANPAGER = "bat --plain";
     PAGER    = "bat --plain";
   };
-  environment.shellAliases = {
-    cat  = "bat";
-    less = "bat --plain";
-  };
-
+  
   home-manager.sharedModules = [{
     programs.bat = enabled {
       config.theme      = "onehalf";
       themes.onehalf.src = pkgs.writeText "onehalf.tmTheme" config.theme.tmTheme;
-
       config.pager = "less --quit-if-one-screen --RAW-CONTROL-CHARS";
+    };
+    
+    # Move shell aliases here
+    home.shellAliases = {
+      cat  = "bat";
+      less = "bat --plain";
     };
   }];
 }
