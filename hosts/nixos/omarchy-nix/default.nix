06beac0275bc90af235b1f51d3e3e0c893c20071
Anwer Khan <deepwatrcreatur@gmail.com>
2025-08-26 16:11:34 -0400

    disable screen lock for new hosts

--- hosts/nixos/omarchy-nix/default.nix
+++ hosts/nixos/omarchy-nix/default.nix
@@ -52,6 +52,17 @@
   # Enable SSH daemon
   services.openssh.enable = true;
 
+  # Disable screen lock
+  services.xserver.displayManager.gdm.autoSuspend = false;
+  security.pam.services.gdm.unixAuth = true;
+  services.logind.lidSwitch = "ignore";
+  services.logind.extraConfig = ''
+    HandlePowerKey=ignore
+    HandleSuspendKey=ignore
+    HandleHibernateKey=ignore
+    HandleLidSwitch=ignore
+  '';
+
   users.users.deepwatrcreatur = {
     isNormalUser = true;
     extraGroups = [ "networkmanager" "wheel" ];
