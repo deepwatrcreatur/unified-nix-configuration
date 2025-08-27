6d2d8afcca1de874304e266d98a1bface9f23c49
Anwer Khan <deepwatrcreatur@gmail.com>
2025-08-27 05:37:43 -0400

    changed path to imported module

--- outputs/workstation.nix
+++ outputs/workstation.nix
@@ -4,5 +4,8 @@
   nixosConfigurations.workstation = helpers.mkNixosSystem {
     system = "x86_64-linux";
     hostPath = ../hosts/nixos/workstation;
+    extraModules = [
+      ../hosts/nixos/default.nix
+    ];
   };
 }
