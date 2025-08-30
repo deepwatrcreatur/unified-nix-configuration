75cf9bd26feee6d9dad5207bfbaf4b543fabd03f
Anwer Khan <deepwatrcreatur@gmail.com>
2025-08-30 12:24:33 -0400

    addressed warnings about deprecated options

--- modules/home-manager/gpg-desktop-linux.nix
+++ modules/home-manager/gpg-desktop-linux.nix
@@ -9,7 +9,7 @@
   services.gpg-agent = {
     enable = true;
     # Choose one based on your desktop environment (e.g., GNOME, KDE, XFCE)
-    pinentryPackage = pkgs.pinentry-gnome3;
-    # Or pinentryPackage = pkgs.pinentry-qt;
+    pinentry.package = pkgs.pinentry-gnome3;
+    # Or pinentry.package = pkgs.pinentry-qt;
   };
 }
