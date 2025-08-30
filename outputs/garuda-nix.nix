a203ae024b4018b77ccf9b30af2e8ff3fee6fced
Anwer Khan <deepwatrcreatur@gmail.com>
2025-08-30 07:06:49 -0400

    removed garuda flake input and output

--- outputs/garuda-nix.nix
+++
@@ -1,8 +0,0 @@
-# outputs/garuda-nix.nix
-{ helpers, ... }:
-{
-  nixosConfigurations.garuda-nix = helpers.mkGarudaSystem {
-    system = "x86_64-linux";
-    hostPath = ../hosts/nixos/garuda-nix;
-  };
-}
