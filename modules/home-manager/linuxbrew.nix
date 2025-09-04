e1d8b6b08e845aee68e0631d75b08d00d2b186cd
Anwer Khan <deepwatrcreatur@gmail.com>
2025-09-04 01:54:42 -0400

    fixed brew installation script that wanted to create linuxbrew directory in /home, since we are now adding it only to user-level config and thus want it in user's home directory

--- modules/home-manager/linuxbrew.nix
+++ modules/home-manager/linuxbrew.nix
@@ -3,29 +3,29 @@
 with lib;
 
 let
-  brewPrefix = "/home/linuxbrew/.linuxbrew";
+  brewPrefix = "$HOME/.linuxbrew";
   commonBrews = (import ../common-brew-packages.nix).brews;
 in
 {
   # Add Homebrew to PATH and environment
-  home.sessionPath = [ "${brewPrefix}/bin" "${brewPrefix}/sbin" ];
+  home.sessionPath = [ "$HOME/.linuxbrew/bin" "$HOME/.linuxbrew/sbin" ];
   
   home.sessionVariables = {
-    HOMEBREW_PREFIX = brewPrefix;
-    HOMEBREW_CELLAR = "${brewPrefix}/Cellar";
-    HOMEBREW_REPOSITORY = "${brewPrefix}/Homebrew";
+    HOMEBREW_PREFIX = "$HOME/.linuxbrew";
+    HOMEBREW_CELLAR = "$HOME/.linuxbrew/Cellar";
+    HOMEBREW_REPOSITORY = "$HOME/.linuxbrew/Homebrew";
   };
 
   # Shell integration
   programs.bash.initExtra = mkIf config.programs.bash.enable ''
-    if [ -f "${brewPrefix}/bin/brew" ]; then
-      eval "$(${brewPrefix}/bin/brew shellenv)"
+    if [ -f "$HOME/.linuxbrew/bin/brew" ]; then
+      eval "$($HOME/.linuxbrew/bin/brew shellenv)"
     fi
   '';
 
   programs.fish.shellInit = mkIf config.programs.fish.enable ''
-    if test -f "${brewPrefix}/bin/brew"
-      eval (${brewPrefix}/bin/brew shellenv)
+    if test -f "$HOME/.linuxbrew/bin/brew"
+      eval ($HOME/.linuxbrew/bin/brew shellenv)
     end
   '';
 
@@ -36,19 +36,23 @@ in
       set -eu
       
       # Install Homebrew if not present
-      if [ ! -f "${brewPrefix}/bin/brew" ]; then
-        echo "Installing Homebrew..."
+      if [ ! -f "$HOME/.linuxbrew/bin/brew" ]; then
+        echo "Installing Homebrew to $HOME/.linuxbrew..."
+        export HOMEBREW_PREFIX="$HOME/.linuxbrew"
         ${pkgs.bash}/bin/bash -c "$(${pkgs.curl}/bin/curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
       fi
 
       # Set up environment
-      export PATH="${brewPrefix}/bin:${brewPrefix}/sbin:$PATH"
+      export PATH="$HOME/.linuxbrew/bin:$HOME/.linuxbrew/sbin:$PATH"
+      export HOMEBREW_PREFIX="$HOME/.linuxbrew"
+      export HOMEBREW_CELLAR="$HOME/.linuxbrew/Cellar"
+      export HOMEBREW_REPOSITORY="$HOME/.linuxbrew/Homebrew"
       
       # Install common packages
       ${concatStringsSep "\n" (map (formula: ''
-        if ! "${brewPrefix}/bin/brew" list "${formula}" &>/dev/null; then
+        if ! "$HOME/.linuxbrew/bin/brew" list "${formula}" &>/dev/null; then
           echo "Installing formula: ${formula}"
-          "${brewPrefix}/bin/brew" install "${formula}"
+          "$HOME/.linuxbrew/bin/brew" install "${formula}"
         fi
       '') commonBrews)}
       
