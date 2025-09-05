4d0e3b3991cd5fb44c812a7f0196bb982df83068
Anwer Khan <deepwatrcreatur@gmail.com>
2025-09-05 13:04:13 -0400

    turned off PAM which was causing problems with SSH login

--- hosts/nixos-lxc/nixos_lxc/modules/users.nix
+++ hosts/nixos-lxc/nixos_lxc/modules/users.nix
@@ -1,11 +1,11 @@
 { config, lib, pkgs, ... }:
 
 {
-  users.users.root.shell = pkgs.nushell;
+  users.users.root.shell = lib.getExe pkgs.bash;
 
   users.users.deepwatrcreatur = {
     isNormalUser = true;
-    shell = pkgs.nushell;
+    shell = lib.getExe pkgs.bash;
     description = "Anwer Khan";
     home = "/home/deepwatrcreatur";
     extraGroups = [ "networkmanager" "wheel" ];
