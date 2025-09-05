10afc3d7ca6f6471a46954184010c62cdaaf8d20
Anwer Khan <deepwatrcreatur@gmail.com>
2025-09-05 05:24:51 -0400

    create two options: build with or without determinate nix, so that initial build in lxc can succeed as bootstrap, add determinate nix after that is done

--- outputs/nixos-lxc.nix
+++ outputs/nixos-lxc.nix
@@ -1,7 +1,34 @@
 # outputs/nixos-lxc.nix
-{ helpers, importAllModulesInDir, ... }:
+{ helpers, importAllModulesInDir, inputs, nixpkgsLib, systemSpecialArgs, homeManagerModuleArgs, commonOverlays, commonNixpkgsConfig, ... }:
 {
-  nixosConfigurations.nixos_lxc = helpers.mkNixosSystem {
+  # LXC configuration without Determinate Nix for initial bootstrap
+  nixosConfigurations.nixos_lxc_without_determinate = nixpkgsLib.nixosSystem {
+    system = "x86_64-linux";
+    specialArgs = systemSpecialArgs;
+    modules = [
+      {
+        nixpkgs.overlays = commonOverlays;
+        nixpkgs.config = commonNixpkgsConfig;
+      }
+      inputs.sops-nix.nixosModules.sops
+      inputs.home-manager.nixosModules.home-manager
+      {
+        home-manager.extraSpecialArgs = homeManagerModuleArgs;
+        home-manager.useGlobalPkgs = true;
+        home-manager.useUserPackages = true;
+        home-manager.sharedModules = [
+          inputs.sops-nix.homeManagerModules.sops
+          inputs.plasma-manager.homeManagerModules.plasma-manager
+        ];
+      }
+      # NOTE: Excluded inputs.determinate.nixosModules.default for LXC compatibility
+      ./modules
+      ../hosts/nixos  # Base NixOS config
+    ] ++ (importAllModulesInDir ../hosts/nixos_lxc/modules);
+  };
+
+  # LXC configuration with Determinate Nix for transition after bootstrap
+  nixosConfigurations.nixos_lxc_with_determinate = helpers.mkNixosSystem {
     system = "x86_64-linux";
     hostPath = ../hosts/nixos;  # Base NixOS config
     modules = importAllModulesInDir ../hosts/nixos_lxc/modules;
