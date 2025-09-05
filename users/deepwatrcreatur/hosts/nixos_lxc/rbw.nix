63862635f2d7829d76f9e55322a2439fa2e91bb1
Anwer Khan <deepwatrcreatur@gmail.com>
2025-09-05 08:22:56 -0400

    updated more paths as needed after modules reorganization

--- users/deepwatrcreatur/hosts/nixos_lxc/rbw.nix
+++
@@ -1,4 +0,0 @@
-{ config, pkgs, ... }:
-{
-  programs.rbw.settings.pinentry = pkgs.pinentry-curses; # macOS-specific pinentry
-}
--- users/root/hosts/nixos_lxc/nh.nix
+++
@@ -1,8 +0,0 @@
-{ config, pkgs, ... }:
-{
-  imports = [../../../../modules/nh.nix];
-  
-  programs.nh = {
-    flake = "/home/deepwatrcreatur/unified-nix-configuration";
-  };
-}
