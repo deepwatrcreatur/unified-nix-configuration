d1042815239899afffdd074849323915085a3875
Anwer Khan <deepwatrcreatur@gmail.com>
2025-09-05 09:37:35 -0400

    Fix dhcpcd configuration and directory creation for LXC

--- hosts/nixos-lxc/nixos_lxc/modules/lxc-networking.nix
+++ hosts/nixos-lxc/nixos_lxc/modules/lxc-networking.nix
@@ -5,7 +5,13 @@
   networking = {
     useDHCP = lib.mkDefault true;
     interfaces.eth0.useDHCP = lib.mkDefault true;
-    dhcpcd.enable = lib.mkForce true;  # Override the global setting
+    dhcpcd = {
+      enable = lib.mkForce true;  # Override the global setting
+      extraConfig = ''
+        interface eth0
+        static domain_name_servers=8.8.8.8 1.1.1.1 10.10.10.1
+      '';
+    };
     nameservers = [ "8.8.8.8" "1.1.1.1" "10.10.10.1" ];
   };
   
@@ -13,6 +19,7 @@
   systemd.services.lxc-networking = {
     description = "LXC Container Networking Setup";
     after = [ "network.target" ];
+    before = [ "dhcpcd.service" ];
     wants = [ "network.target" ];
     wantedBy = [ "multi-user.target" ];
     
@@ -20,13 +27,11 @@
       Type = "oneshot";
       RemainAfterExit = true;
       ExecStart = pkgs.writeShellScript "lxc-network-setup" ''
+        # Create necessary directories
+        mkdir -p /var/run/dhcpcd
+        
         # Bring up interface
         ${pkgs.iproute2}/bin/ip link set eth0 up
-        
-        # Start DHCP if not already running
-        if ! ${pkgs.dhcpcd}/bin/dhcpcd -T eth0 >/dev/null 2>&1; then
-          ${pkgs.dhcpcd}/bin/dhcpcd eth0
-        fi
       '';
     };
   };
