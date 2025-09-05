1b2453a085c6ca488fed89a4ddba49595840b54a
Anwer Khan <deepwatrcreatur@gmail.com>
2025-09-05 09:46:00 -0400

    Fix IPv4 DHCP for LXC containers

--- hosts/nixos-lxc/nixos_lxc/modules/lxc-networking.nix
+++ hosts/nixos-lxc/nixos_lxc/modules/lxc-networking.nix
@@ -8,11 +8,15 @@
     dhcpcd = {
       enable = lib.mkForce true;  # Override the global setting
       extraConfig = ''
+        # Enable both IPv4 and IPv6
+        ipv4
+        ipv6
+        
         interface eth0
+        # Request IPv4 address via DHCP
         static domain_name_servers=8.8.8.8 1.1.1.1 10.10.10.1
       '';
     };
-    nameservers = [ "8.8.8.8" "1.1.1.1" "10.10.10.1" ];
   };
   
   # Ensure proper network startup
@@ -32,6 +36,15 @@
         
         # Bring up interface
         ${pkgs.iproute2}/bin/ip link set eth0 up
+        
+        # Force IPv4 DHCP after interface is up
+        sleep 2
+        ${pkgs.dhcpcd}/bin/dhcpcd -4 eth0 || true
+        
+        # Ensure DNS is set up
+        echo "nameserver 8.8.8.8" > /etc/resolv.conf
+        echo "nameserver 1.1.1.1" >> /etc/resolv.conf
+        echo "nameserver 10.10.10.1" >> /etc/resolv.conf
       '';
     };
   };
