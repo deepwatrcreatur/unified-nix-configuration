31928a616832c687ab5022a005fe0ab318343e52
Anwer Khan <deepwatrcreatur@gmail.com>
2025-09-05 08:04:52 -0400

    reorganizing of lxc folders, addition of networking module for basic lxc

---
+++ hosts/nixos-lxc/nixos_lxc/default.nix
@@ -0,0 +1,28 @@
+
+{ config, lib, inputs, ... }: 
+let
+  nixpkgsLib = inputs.nixpkgs.lib;
+  importAllModulesInDir = dir:
+    let
+      items = builtins.readDir dir;
+      isNixFile = name: type: type == "regular" && nixpkgsLib.hasSuffix ".nix" name;
+      nixFileNames = nixpkgsLib.attrNames (nixpkgsLib.filterAttrs isNixFile items);
+    in
+      map (fileName: dir + "/${fileName}") nixFileNames;
+in
+{
+  imports = [
+    inputs.sops-nix.nixosModules.sops
+    inputs.home-manager.nixosModules.home-manager
+    (./. + "/../../../modules")
+    (./. + "/../../../hosts/nixos")
+  ]
+  ++ (importAllModulesInDir (./. + "/../../../hosts/nixos_lxc/modules"));
+
+  nixpkgs = {
+    overlays = [];
+    config = {
+      allowUnfree = true;
+    };
+  };
+}
