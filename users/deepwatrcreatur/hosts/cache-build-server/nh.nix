f7be6847e729ce505835c0e7cee93c447dc3e183
Anwer Khan <deepwatrcreatur@gmail.com>
2025-09-09 02:47:58 -0400

    corrected more paths to reflect folder reorganization

--- users/deepwatrcreatur/hosts/cache-build-server/nh.nix
+++ users/deepwatrcreatur/hosts/cache-build-server/nh.nix
@@ -1,6 +1,10 @@
 { config, pkgs, ... }:
+
 {
+  imports = [../../../../modules/nh.nix];
+  
   programs.nh = {
+    flake = "/home/deepwatrcreatur/unified-nix-configuration";
     enable = true;
     clean.enable = true;
     clean.extraArgs = "--keep-since 4d --keep 3";
