f731068f676e655450b65d5c9900673ad1548cfa
Anwer Khan <deepwatrcreatur@gmail.com>
2025-09-11 06:35:52 -0400

    only manage authorized_keys on nixos hosts, using built-in host attribute

---
+++ modules/nixos/common/ssh-keys.nix
@@ -0,0 +1,21 @@
+{ config, lib, ... }:
+
+let
+  # Get all .pub files from the ssh-keys directory
+  sshKeysDir = ../../../ssh-keys;
+  pubKeyFiles = builtins.attrNames (lib.filterAttrs 
+    (name: type: type == "regular" && lib.hasSuffix ".pub" name)
+    (builtins.readDir sshKeysDir));
+  
+  # Read content of each public key file and clean whitespace
+  pubKeys = map (file: lib.strings.trim (builtins.readFile (sshKeysDir + "/${file}"))) pubKeyFiles;
+in
+{
+  # Set SSH authorized keys for the main user
+  users.users.deepwatrcreatur = {
+    openssh.authorizedKeys.keys = pubKeys;
+  };
+  
+  # Ensure SSH service is enabled
+  services.openssh.enable = lib.mkDefault true;
+}
