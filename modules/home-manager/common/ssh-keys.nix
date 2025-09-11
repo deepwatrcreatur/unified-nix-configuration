4a1d760481266234b70dc81565718142bf177a5d
Anwer Khan <deepwatrcreatur@gmail.com>
2025-09-11 06:35:52 -0400

    only manage authorized_keys on nixos hosts, using built-in host attribute

--- modules/home-manager/common/ssh-keys.nix
+++ modules/home-manager/common/ssh-keys.nix
@@ -1,33 +1,7 @@
+# This module is deprecated - SSH keys should be managed at the system level
+# in individual host configurations using users.users.<name>.openssh.authorizedKeys.keys
 { config, lib, ... }:
-
-let
-  # Get all .pub files from the ssh-keys directory  
-  sshKeysDir = ../../../ssh-keys;
-  
-  # Debug: ensure directory exists and list contents
-  pubKeyFiles = builtins.attrNames (lib.filterAttrs 
-    (name: type: type == "regular" && lib.hasSuffix ".pub" name)
-    (builtins.readDir sshKeysDir));
-  
-  # Read content of each public key file
-  pubKeys = map (file: builtins.readFile (sshKeysDir + "/${file}")) pubKeyFiles;
-  
-  # Debug output
-  debugInfo = "# SSH Keys loaded from ${toString sshKeysDir}\n# Found files: ${toString pubKeyFiles}\n";
-in
 {
-  # Home Manager way to set SSH authorized keys  
-  home.file.".ssh/authorized_keys".text = debugInfo + lib.concatStringsSep "\n" pubKeys;
-  
-  # Use activation script to fix SSH permissions after files are created
-  home.activation.fixSshPermissions = lib.hm.dag.entryAfter ["writeBoundary"] ''
-    if [[ ! -v DRY_RUN ]]; then
-      mkdir -p $HOME/.ssh
-      chmod 700 $HOME/.ssh
-      if [[ -f $HOME/.ssh/authorized_keys ]]; then
-        chmod 600 $HOME/.ssh/authorized_keys
-        echo "Fixed SSH permissions: .ssh (700), authorized_keys (600)"
-      fi
-    fi
-  '';
+  # This file intentionally left mostly empty
+  # SSH keys are now managed in host configurations
 }
