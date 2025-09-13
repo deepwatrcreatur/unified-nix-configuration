89e1e89a5d555b5c4dbb0fdfb10d9dedaffaffef
Anwer Khan <deepwatrcreatur@gmail.com>
2025-09-13 00:33:07 -0400

    added import of all modules for inference VMs

---
+++ hosts/nixos/inference-vm/default.nix
@@ -0,0 +1,18 @@
+{ config, pkgs, lib, inputs, ... }:
+
+let
+  # Import all .nix modules from the modules directory
+  importAllModulesInDir = dir:
+    let
+      items = builtins.readDir dir;
+      isNixFile = name: type: type == "regular" && lib.hasSuffix ".nix" name;
+      nixFileNames = lib.attrNames (lib.filterAttrs isNixFile items);
+    in
+      map (fileName: dir + "/${fileName}") nixFileNames;
+in
+{
+  # Common configuration for all inference VMs
+  # This automatically imports all modules from the modules/ subdirectory
+  
+  imports = importAllModulesInDir ./modules;
+}
