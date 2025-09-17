260f89ca6264eb00a60f57d30d9d111a8229dcac
Anwer Khan <deepwatrcreatur@gmail.com>
2025-09-17 12:12:57 -0400

    use version 25.05 of nix-darwin to conform to rest of repo

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
 
-    nix-darwin.url = "github:LnL7/nix-darwin/master";
+    nix-darwin.url = "github:LnL7/nix-darwin/release-25.05";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
 
     home-manager.url = "github:nix-community/home-manager/release-25.05";
