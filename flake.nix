13aff1bfa9a5aa95ba3204dd00a14672f6b68978
Anwer Khan <deepwatrcreatur@gmail.com>
2025-09-17 12:40:03 -0400

    updated flake input to match versions

--- flake.nix
+++ flake.nix
@@ -3,9 +3,9 @@
   description = "Multi-system Nix configurations (NixOS, nix-darwin, Home Manager)";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
-    nix-darwin.url = "github:LnL7/nix-darwin/release-25.05";
+    nix-darwin.url = "github:LnL7/nix-darwin/master";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
 
     home-manager.url = "github:nix-community/home-manager/release-25.05";
