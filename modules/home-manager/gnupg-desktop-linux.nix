6ac7dc514529e23c697cf59969cb95134d5ee51a
Anwer Khan <deepwatrcreatur@gmail.com>
2025-10-03 00:20:00 -0400

    pick just one gpg agent for workstation to avoid conflict

--- modules/home-manager/gnupg-desktop-linux.nix
+++ modules/home-manager/gnupg-desktop-linux.nix
@@ -2,12 +2,12 @@
 { config, pkgs, lib, inputs, ... }:
 
 {
-  imports = [ inputs.home-manager.modules.programs.gnupg ];`:q`  
   programs.gnupg = {
     enable = true;
-    pinentry.enable = true;
-    # Choose one based on your desktop environment (e.g., GNOME, KDE, XFCE)
-    pinentry.program = "${pkgs.pinentry-gtk2}/bin/pinentry-gtk2";
-    # Or pinentry.program = "${pkgs.pinentry-qt}/bin/pinentry-qt";
+    agent = {
+      enable = true;
+      enableSshSupport = true;
+      pinentry.program = "${pkgs.pinentry-gnome3}/bin/pinentry";
+    };
   };
 }
