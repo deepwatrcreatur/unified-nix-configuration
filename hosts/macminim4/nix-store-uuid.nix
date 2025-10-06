c8fdae0d6e48b6053d613d33916602c1cb0be352
Anwer Khan <deepwatrcreatur@gmail.com>
2025-10-06 02:16:05 -0400

    updated UUID for Nix_Store on macminim4 after wiping and reinstall

--- hosts/macminim4/nix-store-uuid.nix
+++ hosts/macminim4/nix-store-uuid.nix
@@ -1,5 +1,5 @@
 # hosts/macminim4/nix-store-uuid.nix
 { config, lib, ... }:
 {
-  custom.nix-mount.uuid = "E1A6C722-457C-4C80-AA6C-098431B3BD0D";
+  custom.nix-mount.uuid = "4B72B090-97DE-46CE-96DC-078B4E5D8891";
 }
