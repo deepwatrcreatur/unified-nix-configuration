52ccfcf31d0255df33056f0a6ce706fc2a8d3eab
Anwer Khan <deepwatrcreatur@gmail.com>
2025-10-06 02:36:42 -0400

    corrected syntax for shell escape

--- modules/nix-darwin/common/nix-mount.nix
+++ modules/nix-darwin/common/nix-mount.nix
@@ -55,7 +55,7 @@ in
       # Update determinate nix mount script with current UUID
       if [ -f /usr/local/bin/mount-nix-ssd.sh ]; then
         echo "Updating determinate nix mount script with UUID: ${cfg.uuid}"
-        /usr/bin/sed -i '' 's/SSD_UUID=".*"/SSD_UUID="${cfg.uuid}"/' /usr/local/bin/mount-nix-ssd.sh || true
+        /usr/bin/sed -i "" "s/SSD_UUID=\".*\"/SSD_UUID=\"${cfg.uuid}\"/" /usr/local/bin/mount-nix-ssd.sh || true
       fi
     '';
   };
