955fdc2950d3e0ea929c3b3a731422e78be86f1c
Anwer Khan <deepwatrcreatur@gmail.com>
2025-10-08 07:17:47 -0400

    removed cached-nix-shell (broken on darwin)

--- modules/common/nix-tools.nix
+++ modules/common/nix-tools.nix
@@ -1,13 +1,13 @@
 { config, pkgs, lib, inputs, ... }: {
 
   environment.systemPackages = with pkgs; [
-    cached-nix-shell
+    # cached-nix-shell  # Broken on macOS due to nokogiri compilation issues
     devenv
     lorri
     nix-output-monitor
     nix-tree
     nixfmt
     nvd
-    statix    
+    statix
   ];
 }
