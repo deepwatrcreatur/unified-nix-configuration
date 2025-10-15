100eafb35b183ed829ea3e7b76b1624d47c7b00a
Anwer Khan <deepwatrcreatur@gmail.com>
2025-10-15 03:08:52 -0400

    added common nixos module to back up files that will be replaced on activation

---
+++ modules/nixos/common/activation-backup.nix
@@ -0,0 +1,17 @@
+# modules/nixos/common/activation-backup.nix
+# Automatically backup existing files that would block system activation
+
+{ config, lib, pkgs, ... }:
+
+{
+  # Backup existing files during activation instead of failing
+  environment.etc."nixos/.keep-etc".text = ''
+    This directory is managed by NixOS.
+  '';
+
+  # Set the backup file extension for files that would conflict during activation
+  environment.etc."backup-extension" = {
+    text = ".backup-before-nix";
+    mode = "0644";
+  };
+}
