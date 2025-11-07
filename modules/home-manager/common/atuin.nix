291fc5a42ec4371ebaec1fce9cb5da4767794968
Anwer Khan <deepwatrcreatur@gmail.com>
2025-11-07 05:22:28 -0500

    fixed atuin module's handling of atuin secret

--- modules/home-manager/common/atuin.nix
+++ modules/home-manager/common/atuin.nix
@@ -1,8 +1,5 @@
 { config, pkgs, lib, ... }:
 
-let
-  secretPath = config.sops.secrets."atuin-key".path;
-in
 {
   programs.atuin = {
     enable = true;
@@ -11,34 +8,9 @@ in
     enableZshIntegration = true;
   };
 
-  # Configure SOPS secret for Atuin key
   sops.secrets."atuin-key" = {
     sopsFile = ../../../secrets/atuin-key.enc;
     key = "atuin_key";
+    path = "${config.home.homeDirectory}/.config/atuin/key.json";
   };
-
-  # Set ATUIN_KEY environment variable by reading the decrypted secret file
-  programs.bash.initExtra = ''
-    if [[ -f "${secretPath}" ]]; then
-      export ATUIN_KEY="$(/bin/cat ${secretPath})"
-    fi
-  '';
-
-  programs.zsh.initContent = ''
-    if [[ -f "${secretPath}" ]]; then
-      export ATUIN_KEY="$(/bin/cat ${secretPath})"
-    fi
-  '';
-
-  programs.fish.interactiveShellInit = ''
-    if test -f ${secretPath}
-      set -gx ATUIN_KEY (/bin/cat ${secretPath})
-    end
-  '';
-
-  programs.nushell.extraConfig = ''
-    if ("${secretPath}" | path exists) {
-      $env.ATUIN_KEY = (open "${secretPath}" | str trim)
-    }
-  '';
 }
