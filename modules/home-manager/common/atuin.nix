0705d214acc63df083c64b832ec5bcce70d14c5c
Anwer Khan <deepwatrcreatur@gmail.com>
2025-11-16 06:07:45 -0500

    added shell alias for atuin

--- modules/home-manager/common/atuin.nix
+++ modules/home-manager/common/atuin.nix
@@ -8,6 +8,14 @@
     enableZshIntegration = true;
   };
 
+  # Add shell alias for atuin script run
+  programs.bash.shellAliases.asr = "atuin script run";
+  programs.zsh.shellAliases.asr = "atuin script run";
+  programs.fish.shellAliases.asr = "atuin script run";
+  programs.nushell.extraConfig = ''
+    alias asr = atuin script run
+  '';
+
   sops.secrets."atuin-key" = {
     sopsFile = ../../../secrets/atuin-key.enc;
     key = "atuin_key";
