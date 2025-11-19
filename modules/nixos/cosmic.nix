4870f22728e51d9bb97a0846d071d3436c197bda
Anwer Khan <deepwatrcreatur@gmail.com>
2025-11-19 10:24:55 -0500

    switched to COSMIC desktop

--- modules/nixos/cosmic.nix
+++ modules/nixos/cosmic.nix
@@ -1,18 +1,12 @@
 { config, pkgs, lib, ... }:
 
 {
-  services.xserver.enable = true;
+  services.desktopManager.cosmic.enable = true;
 
-  services.xserver.desktopManager.cosmic.enable = true;
-
-  services.xserver.displayManager.gdm = {
-    enable = true;
-    defaultSession = "cosmic";
-  };
+  services.displayManager.cosmic-greeter.enable = true;
 
   # Only add packages not automatically included by cosmic desktop manager
   environment.systemPackages = with pkgs; [
-    cosmic-ext-tweaks  # Extension for additional COSMIC tweaks
-    cosmic-ext-ctl     # Control utility for COSMIC extensions
+    # No extra cosmic extensions for now, as they might be outdated
   ];
 }
