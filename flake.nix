5ba7e0d15570b02675c8a16fa89add5235f0e219
Anwer Khan <deepwatrcreatur@gmail.com>
2025-11-27 00:30:20 -0500

    feat(nix): re-introduce backupFileExtension for NixOS hosts
    This commit re-introduces the  option for NixOS
    hosts. This option is still supported in the version of home-manager
    used for NixOS builds.

--- flake.nix
+++ flake.nix
@@ -235,5 +235,12 @@
         ) {} outputFiles;
 
   in
-    loadOutputs ./outputs;
+    (loadOutputs ./outputs) // {
+      hm-opts = inputs.home-manager.lib.homeManagerConfiguration {
+        pkgs = import inputs.nixpkgs {
+          system = "x86_64-linux";
+        };
+        modules = [ ./modules/home-manager/non-nixos.nix ];
+      };
+    };
 }
