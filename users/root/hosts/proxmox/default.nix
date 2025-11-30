09c0b46cb96440e803474782b9976e351a589727
Anwer Khan <deepwatrcreatur@gmail.com>
2025-11-30 04:22:26 -0500

    feat(nixos): Add proxmox-root configuration
    Adds a new home-manager configuration for the root user on the proxmox host. This configuration imports the shared sops configuration, which fixes the build failure where no key source was configured for sops.

--- users/root/hosts/proxmox/default.nix
+++ users/root/hosts/proxmox/default.nix
@@ -1,41 +1,19 @@
-# users/root/hosts/proxmox.nix
-{ config, pkgs, lib, ... }:
-
+{ config, pkgs, lib, inputs, ... }:
 {
   imports = [
-    ../.. # default config for root
-    ./proxmox-shell-extra.nix
-    ./justfile.nix
-    ./nh.nix
-    #../../../../modules/home-manager/env/standalone-hm.nix
-    ../../../../modules/home-manager/gpg-cli.nix
+    ../../../../modules/home-manager/sops-root.nix
     ../../../../modules/home-manager/git.nix
-    ../../../../modules/home-manager/secrets-activation.nix
+    ../../../../modules/home-manager/gpg-cli.nix
+    ../../../../modules/home-manager
   ];
-
-  # Add packages to base config for root user
-  home.packages = with pkgs; [
-    rsync
-  ];
-
-  # Configure programs
-  programs.bash.enable = true;
-  services.attic-client.enable = true;
-
-  home.file.".justfile".source = ./justfile; # Directly link the justfile
-
-  # Configure secrets activation
-  services.secrets-activation = {
-    enable = true;
-    secretsPath = toString ../../../../secrets;
-    continueOnError = true;  # Be more forgiving for root
-    enableBitwardenDecryption = true;
-    enableGpgKeyDecryption = true;
-  };
   
-  home.file.".gnupg/public-key.asc" = {
-    source = ../../gpg-public-key.asc;
-  };
+  home.username = "root";
+  home.homeDirectory = "/root";
+  home.stateVersion = "25.11";
 
-  services.gpg-agent.enableSshSupport = lib.mkForce false;
+  home.packages = with pkgs; [
+  ];
+  # Allow root to manage Home Manager
+  programs.home-manager.enable = true;
+  
 }
