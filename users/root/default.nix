f2fc91c7e8fa871ff832de9f7206f641ec523a9f
Anwer Khan <deepwatrcreatur@gmail.com>
2025-11-30 04:05:58 -0500

    feat(sops): use custom activation script for root user

--- users/root/default.nix
+++ users/root/default.nix
@@ -1,8 +1,8 @@
 { config, pkgs, lib, inputs, ... }:
 {
   imports = [
-    inputs.sops-nix.homeManagerModules.sops
-    ../../modules/home-manager/sops-root.nix
+    # inputs.sops-nix.homeManagerModules.sops
+    # ../../modules/home-manager/sops-root.nix
     ../../modules/home-manager/git.nix
     ../../modules/home-manager/gpg-cli.nix
     ../../modules/home-manager
@@ -16,5 +16,14 @@
   ];
   # Allow root to manage Home Manager
   programs.home-manager.enable = true;
-  
+
+  home.activation.mySopsActivation = lib.hm.dag.entryAfter ["writeBoundary"] ''
+    export SOPS_AGE_KEY_FILE="$HOME/.config/sops/age/keys.txt"
+    export PATH="${lib.makeBinPath [ pkgs.sops ]}:$PATH"
+
+    # Decrypt github-token
+    mkdir -p "$HOME/.config/git"
+    sops -d "${toString ./secrets}/github-token.txt.enc" > "$HOME/.config/git/github-token"
+    chmod 600 "$HOME/.config/git/github-token"
+  '';
 }
