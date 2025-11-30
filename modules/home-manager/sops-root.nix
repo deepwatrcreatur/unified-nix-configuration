029f843c3c6b151841afd590555b8dc9cb10ee28
Anwer Khan <deepwatrcreatur@gmail.com>
2025-11-30 04:02:04 -0500

    fix(sops): fix github-token path for root user

--- modules/home-manager/sops-root.nix
+++ modules/home-manager/sops-root.nix
@@ -1,15 +1,8 @@
 # modules/home-manager/sops-root.nix
 { config, pkgs, lib, inputs, ... }:
 
-let
-  globalSopsSecretsDir = toString (builtins.path { path = ../../secrets; });
-in
-{
-  sops.age.keyFile = "${config.home.homeDirectory}/.config/sops/age/keys.txt";
-
   sops.secrets."github-token-root" = {
-    sopsFile = "${globalSopsSecretsDir}/github-token.txt.enc";
+    sopsFile = ../../users/root/secrets/github-token.txt.enc;
     path = "${config.home.homeDirectory}/.config/git/github-token";
     mode = "0600";
   };
-}
