e07dbc03997cb643114beaf97dc364346d2ff9cd
Anwer Khan <deepwatrcreatur@gmail.com>
2025-11-30 04:03:20 -0500

    fix(sops): fix syntax error in sops-root.nix

--- modules/home-manager/sops-root.nix
+++ modules/home-manager/sops-root.nix
@@ -1,8 +1,10 @@
 # modules/home-manager/sops-root.nix
 { config, pkgs, lib, inputs, ... }:
 
+{
   sops.secrets."github-token-root" = {
     sopsFile = ../../users/root/secrets/github-token.txt.enc;
     path = "${config.home.homeDirectory}/.config/git/github-token";
     mode = "0600";
   };
+}
