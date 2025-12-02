ea8924a0a1f85a7c4b11a413601d7d40f6d84ed0
root <deepwatrcreatur@gmail.com>
2025-12-02 06:05:00 -0500

    Fix: Complete sops configuration for proxmox hosts
    Updated modules/home-manager/sops-root.nix to include full sops-nix import,
    sops package, and github-token-root secret definition. This resolves
    the 'github-token-root not found' issue on proxmox hosts by ensuring
    the configuration is complete and aligned with other sops setups.

--- modules/home-manager/sops-root.nix
+++ modules/home-manager/sops-root.nix
@@ -2,11 +2,22 @@
 { config, pkgs, lib, inputs, ... }:
 
 {
+  imports = [
+    inputs.sops-nix.homeManagerModules.sops
+  ];
+
+  home.packages = with pkgs; [
+    sops
+  ];
+
   sops.age.keyFile = "${config.home.homeDirectory}/.config/sops/age/keys.txt";
 
   sops.secrets."github-token-root" = {
     sopsFile = ../../users/root/secrets/github-token.txt.enc;
-    path = "${config.home.homeDirectory}/.config/git/github-token";
-    mode = "0600";
+    format = "binary";
   };
+
+  home.file.".config/fish/conf.d/github-token.fish".text = ''
+    set -x GITHUB_TOKEN (cat ${config.sops.secrets."github-token-root".path})
+  '';
 }
