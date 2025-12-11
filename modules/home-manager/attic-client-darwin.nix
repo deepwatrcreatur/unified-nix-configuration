efecc0fea27ca87fcd5356d767ae403f746c0cb8
Anwer Khan <deepwatrcreatur@gmail.com>
2025-12-11 02:34:54 -0500

    fix: remove attic-client service reference on macOS
    - attic-client service only exists on NixOS/Linux systems
    - On macOS, use programs.attic-client instead of services.attic-client
    - Fixes build error on macminim4 where service doesn't exist

--- modules/home-manager/attic-client-darwin.nix
+++ modules/home-manager/attic-client-darwin.nix
@@ -1,9 +1,9 @@
 { config, lib, ... }:
 
 {
-  # Enable attic-client by default on macOS/Darwin systems
-  # This overrides the default from the common module
-  services.attic-client.enable = lib.mkDefault true;
+  # attic-client service is only available on NixOS/Linux systems
+  # On macOS, we use the attic-client package directly without a service
+  programs.attic-client.enable = lib.mkDefault true;
 
   # Enable user Nix configuration for Determinate Nix systems
   services.nix-user-config.enable = lib.mkDefault true;
