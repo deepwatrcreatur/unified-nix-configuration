89b332a39316ed501c880f348af6661bd2819388
Anwer Khan <deepwatrcreatur@gmail.com>
2025-12-25 06:40:39 -0500

    feat(home-manager): integrate nix-index-database for enhanced comma functionality
    - Add nix-index-database flake input for pre-built package index
    - Update comma.nix to use centralized nix-index-database module
    - Enable both nix-index and comma with shared database
    - Remove redundant configuration in favor of integrated approach

--- modules/home-manager/common/comma.nix
+++ modules/home-manager/common/comma.nix
@@ -1,7 +1,13 @@
 # modules/home-manager/common/comma.nix
+{ inputs, ... }:
+
 {
-  ...
-}:
-{
+  imports = [
+    inputs.nix-index-database.hmModules.nix-index
+  ];
+
+  # This replaces the need for programs.comma.enable = true;
+  # as it configures both nix-index and comma together.
   programs.nix-index-database.comma.enable = true;
+  programs.nix-index.enable = true;
 }
