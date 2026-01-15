c1540aa1683658db70634a004bbf742baa32791f
Anwer Khan <deepwatrcreatur@gmail.com>
2026-01-15 05:17:56 -0500

    fix(lxc): disable growPartition in containers

--- hosts/nixos-lxc/homeserver/configuration.nix
+++ hosts/nixos-lxc/homeserver/configuration.nix
@@ -35,6 +35,9 @@
   ];
 
   boot.initrd.systemd.fido2.enable = false;
-  boot.growPartition = true;
+
+  # LXC containers don't have a stable block device for `fileSystems."/".device`.
+  # NixOS grow-partition relies on that, so disable it here.
+  boot.growPartition = false;
 
 }
