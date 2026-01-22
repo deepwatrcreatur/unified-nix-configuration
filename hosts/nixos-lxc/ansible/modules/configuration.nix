ef78ce171a5268cf3bf59e828155b94d0e18e952
Anwer Khan <deepwatrcreatur@gmail.com>
2026-01-22 01:27:01 -0500

    refactor(lxc): remove boot.growPartition from container configs

--- hosts/nixos-lxc/ansible/modules/configuration.nix
+++ hosts/nixos-lxc/ansible/modules/configuration.nix
@@ -11,10 +11,6 @@
 
   networking.hostName = "ansible";
 
-  # LXC containers don't have a stable block device for `fileSystems."/".device`.
-  # NixOS grow-partition relies on that, so disable it here.
-  boot.growPartition = false;
-
   boot.initrd.systemd.fido2.enable = false;
 
   system.stateVersion = "25.05";
