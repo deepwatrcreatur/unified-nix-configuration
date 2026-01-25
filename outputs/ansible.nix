7c032da76a0f662ea94d27c8ea896a519f7fe0ca
Anwer Khan <deepwatrcreatur@gmail.com>
2026-01-25 09:17:33 -0500

    corrected module import path

--- outputs/ansible.nix
+++ outputs/ansible.nix
@@ -6,19 +6,27 @@
   ...
 }:
 {
-  #nixosConfigurations.ansible = helpers.mkNixosSystem {
-  #  system = "x86_64-linux";
-  #  hostPath = ../hosts/nixos-lxc/ansible;
-  #  extraModules = [
-  #    ../hosts/nixos-lxc/ansible/sops.nix
-  #    ({ config, lib, inputs, ... }: {
-  #      home-manager.users.ansible = {
-  #        imports = [ ../modules/home-manager ];
-  #        home.stateVersion = "24.11";
-  #        sops.age.keyFile = "/var/lib/sops/age/key.txt";
-  #      };
-  #      home-manager.extraSpecialArgs = homeManagerModuleArgs;
-  #    })
-  #  ];
-  #};
+  nixosConfigurations.ansible = helpers.mkNixosSystem {
+    system = "x86_64-linux";
+    hostPath = ../hosts/nixos-lxc/ansible;
+    extraModules = [
+      ../hosts/nixos-lxc/ansible/modules/sops.nix
+      (
+        {
+          config,
+          lib,
+          inputs,
+          ...
+        }:
+        {
+          sops.age.keyFile = "/var/lib/sops/age/key.txt";
+          home-manager.users.ansible = {
+            imports = [ ../modules/home-manager ];
+            home.stateVersion = "24.11";
+          };
+          home-manager.extraSpecialArgs = homeManagerModuleArgs;
+        }
+      )
+    ];
+  };
 }
