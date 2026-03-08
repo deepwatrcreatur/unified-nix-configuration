34ec59f3ee1b8f2c4568ff5d2130acc7eb58574e
Anwer Khan <deepwatrcreatur@gmail.com>
2026-03-08 04:56:46 -0400

    fix(gateway): correct home-manager config structure

--- users/deepwatrcreatur/hosts/gateway/default.nix
+++ users/deepwatrcreatur/hosts/gateway/default.nix
@@ -6,55 +6,47 @@
 }:
 
 {
-  # Home manager configuration for gateway
-  home-manager.users.deepwatrcreatur = {
-    imports = [
-      ./nix-darwin # Import darwin-specific config (for macOS consistency)
-    ];
+  home.packages = with pkgs; [
+    # Common utilities
+    git
+    vim
+    wget
+    curl
+    rsync
+    htop
+    neofetch
 
-    home.packages = with pkgs; [
-      # Common utilities
-      git
-      vim
-      wget
-      curl
-      rsync
-      htop
-      neofetch
+    # Network utilities
+    nmap
+    tcpdump
+    iperf3
 
-      # Network utilities
-      nmap
-      tcpdump
-      iperf3
+    # System monitoring
+    glances
 
-      # System monitoring
-      neofetch
-      glances
+    # SSH tools
+    mosh
+  ];
 
-      # SSH tools
-      mosh
-    ];
+  # Terminal configuration
+  programs.fish = {
+    enable = true;
+    shellInit = ''
+      set fish_greeting
 
-    # Terminal configuration
-    programs.fish = {
-      enable = true;
-      shellInit = ''
-        set fish_greeting
+      # Aliases
+      alias ls='eza --icons=auto'
+      alias ll='eza -l --icons=auto'
+      alias la='eza -la --icons=auto'
+      alias grep='rg'
+      alias cat='bat'
+    '';
+  };
 
-        # Aliases
-        alias ls='eza --icons=auto'
-        alias ll='eza -l --icons=auto'
-        alias la='eza -la --icons=auto'
-        alias grep='rg'
-        alias cat='bat'
-      '';
-    };
-
-    # Editor configuration
-    programs.neovim = {
-      enable = true;
-      viAlias = true;
-      vimAlias = true;
-    };
+  # Editor configuration
+  programs.neovim = {
+    enable = true;
+    viAlias = true;
+    vimAlias = true;
   };
 }
