f636ad1b09fba37c4835950991ec8936fc967c61
Anwer Khan <deepwatrcreatur@gmail.com>
2026-03-11 00:43:47 -0400

    fix: make known_hosts_dynamic truly mutable
    - Use activation script instead of home.file to avoid nix store symlink
    - Creates regular file that SSH can write to
    - Preserves existing keys during migration
    - Now SSH can actually append new hosts dynamically

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -31,16 +31,23 @@ in
   };
   home.file.".ssh/config".source = ./ssh-config;
   
-  # Hybrid known_hosts: Create empty dynamic file for SSH to populate
-  home.file.".ssh/known_hosts_dynamic" = {
-    text = "# Dynamic known_hosts - SSH will write new host keys here\n";
-    # Don't force - let SSH append to it
-  };
+  # Hybrid known_hosts: Use activation script to create mutable file
+  # Can't use home.file because it creates immutable nix store symlink
+  home.activation.setupDynamicKnownHosts = lib.hm.dag.entryAfter ["writeBoundary"] ''
+    # Create mutable known_hosts_dynamic if it doesn't exist
+    if [[ ! -f ~/.ssh/known_hosts_dynamic ]] || [[ -L ~/.ssh/known_hosts_dynamic ]]; then
+      $DRY_RUN_CMD rm -f ~/.ssh/known_hosts_dynamic
+      $DRY_RUN_CMD touch ~/.ssh/known_hosts_dynamic
+      $DRY_RUN_CMD chmod 600 ~/.ssh/known_hosts_dynamic
+      echo "Created mutable known_hosts_dynamic"
+    fi
+  '';
   
-  home.activation.migrateKnownHosts = lib.hm.dag.entryAfter ["writeBoundary"] ''
+  home.activation.migrateKnownHosts = lib.hm.dag.entryAfter ["setupDynamicKnownHosts"] ''
     # One-time migration: move existing known_hosts to known_hosts_dynamic
     if [[ -f ~/.ssh/known_hosts ]] && [[ ! -f ~/.ssh/known_hosts.migrated ]]; then
-      $DRY_RUN_CMD mv -v ~/.ssh/known_hosts ~/.ssh/known_hosts_dynamic
+      $DRY_RUN_CMD cat ~/.ssh/known_hosts >> ~/.ssh/known_hosts_dynamic
+      $DRY_RUN_CMD mv ~/.ssh/known_hosts ~/.ssh/known_hosts.bak
       $DRY_RUN_CMD touch ~/.ssh/known_hosts.migrated
       echo "Migrated existing known_hosts to known_hosts_dynamic"
     fi
