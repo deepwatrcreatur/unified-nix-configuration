7e94ff8ceb999f766256442374f3c95c02125c49
Anwer Khan <deepwatrcreatur@gmail.com>
2026-03-16 06:47:24 -0400

    Use merged fnox main branch

--- flake.nix
+++ flake.nix
@@ -74,7 +74,7 @@
     };
 
     fnox = {
-      url = "git+ssh://git@github.com/deepwatrcreatur/fnox-flake?ref=feat/modularize-fnox-flake";
+      url = "git+ssh://git@github.com/deepwatrcreatur/fnox-flake";
       inputs.nixpkgs.follows = "nixpkgs";
       flake = true;
     };
