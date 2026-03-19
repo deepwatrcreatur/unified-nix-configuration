60c9d22793efec2c7a084106820aeb959e47d88d
Anwer Khan <deepwatrcreatur@gmail.com>
2026-03-19 08:07:53 -0400

    Enable insecure mode for homeserver homebridge

--- hosts/nixos-lxc/homeserver/modules/homebridge.nix
+++ hosts/nixos-lxc/homeserver/modules/homebridge.nix
@@ -16,4 +16,9 @@
 
     uiSettings.port = 8581;
   };
+
+  # homebridge-alexa needs unauthenticated accessory access, which Homebridge
+  # enables through the UIX_INSECURE_MODE environment variable when using
+  # homebridge-config-ui-x / hb-service.
+  systemd.services.homebridge.environment.UIX_INSECURE_MODE = "1";
 }
