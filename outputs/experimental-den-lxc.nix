7379abe48eb0e215b285a9e833783ef3e2d2e5ab
Anwer Khan <deepwatrcreatur@gmail.com>
2026-03-20 11:59:18 -0400

    prototype: expand den output coverage

--- outputs/experimental-den-lxc.nix
+++ outputs/experimental-den-lxc.nix
@@ -1,6 +1,12 @@
 {
+  commonNixpkgsConfig,
+  commonOverlays,
   helpers,
+  homeManagerModuleArgs,
+  importAllModulesInDir,
+  inputs,
   nixpkgsLib,
+  systemSpecialArgs,
   ...
 }:
 let
@@ -9,11 +15,38 @@ let
   };
 
   inventory = import ../experiments/den-lxc/inventory;
+
+  nixosOutputs = nixpkgsLib.mapAttrsToList (
+    _: host: framework.mkNixosOutput (host // { outputName = "${host.name}-den"; })
+  ) inventory.hosts;
+
+  homeOutputs = nixpkgsLib.mapAttrsToList (
+    _: home: framework.mkHomeOutput (home // { outputName = "${home.name}-den"; })
+  ) inventory.homes;
+
+  darwinOutputs = nixpkgsLib.mapAttrsToList (
+    _: host: framework.mkDarwinOutput (host // { outputName = "${host.name}-den"; })
+  ) inventory.darwin;
+
+  bootstrapOutputs =
+    let
+      legacy = import ../outputs/nixos-lxc.nix {
+        inherit
+          helpers
+          importAllModulesInDir
+          inputs
+          nixpkgsLib
+          systemSpecialArgs
+          homeManagerModuleArgs
+          commonOverlays
+          commonNixpkgsConfig
+          ;
+      };
+    in
+    {
+      nixosConfigurations = nixpkgsLib.mapAttrs' (
+        name: value: nixpkgsLib.nameValuePair "${name}-den" value
+      ) legacy.nixosConfigurations;
+    };
 in
-helpers.mergeOutputs [
-  (framework.mkNixosOutput (inventory.hosts.attic-cache // { outputName = "attic-cache-den"; }))
-  (framework.mkNixosOutput (inventory.hosts.gateway // { outputName = "gateway-den"; }))
-  (framework.mkNixosOutput (inventory.hosts.homeserver // { outputName = "homeserver-den"; }))
-  (framework.mkNixosOutput (inventory.hosts.podman // { outputName = "podman-den"; }))
-  (framework.mkHomeOutput (inventory.homes.proxmox-root // { outputName = "proxmox-root-den"; }))
-]
+helpers.mergeOutputs (nixosOutputs ++ homeOutputs ++ darwinOutputs ++ [ bootstrapOutputs ])
