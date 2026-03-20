ed1e6b622aa718966b9aff4f694e75d800fc7857
Anwer Khan <deepwatrcreatur@gmail.com>
2026-03-20 11:37:28 -0400

    prototype: add gateway test output

--- outputs/experimental-den-lxc.nix
+++ outputs/experimental-den-lxc.nix
@@ -12,6 +12,7 @@ let
 in
 helpers.mergeOutputs [
   (framework.mkNixosOutput (inventory.hosts.attic-cache // { outputName = "attic-cache-den"; }))
+  (framework.mkNixosOutput (inventory.hosts.gateway // { outputName = "gateway-den"; }))
   (framework.mkNixosOutput (inventory.hosts.homeserver // { outputName = "homeserver-den"; }))
   (framework.mkNixosOutput (inventory.hosts.podman // { outputName = "podman-den"; }))
   (framework.mkHomeOutput (inventory.homes.proxmox-root // { outputName = "proxmox-root-den"; }))
