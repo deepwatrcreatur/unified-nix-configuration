daf659f80404e5821f15a3ac3e7a06f74889094a
Anwer Khan <deepwatrcreatur@gmail.com>
2026-03-27 15:30:33 -0400

    fix(podman): restore migrated container stacks

--- den/aspects/podman-containers.nix
+++ den/aspects/podman-containers.nix
@@ -4,5 +4,46 @@
   imports = [
     ../../modules/nixos/container-stack.nix
     ../../hosts/nixos-lxc/podman/stacks/plex-stack.nix
+    ../../hosts/nixos-lxc/podman/stacks/nightscout-stack.nix
+    ../../hosts/nixos-lxc/podman/stacks/paperless-stack.nix
+    ../../hosts/nixos-lxc/podman/stacks/librelinkup-stack.nix
   ];
+
+  # Enable Podman with settings
+  virtualisation.podman = {
+    enable = true;
+    dockerCompat = true;
+    defaultNetwork.settings.dns_enabled = true;
+    autoPrune.enable = true;
+  };
+
+  # Declarative OCI containers managed by systemd
+  virtualisation.oci-containers = {
+    backend = "podman";
+  };
+
+  # Create persistent data directories
+  systemd.tmpfiles.rules = [
+    "d /var/lib/plex 0777 root root -"
+    "d /var/lib/plex/config 0777 root root -"
+    "d /var/lib/plex/transcode 0777 root root -"
+  ];
+
+  # Open firewall ports
+  networking.firewall = {
+    allowedTCPPorts = [
+      11337 # Nightscout
+      32400 # Plex
+      3005 # Plex
+      8324 # Plex
+      32469 # Plex
+    ];
+    allowedUDPPorts = [
+      1900 # Plex DLNA
+      32410 # Plex GDM
+      32412 # Plex GDM
+      32413 # Plex GDM
+      32414 # Plex GDM
+    ];
+  };
 }
