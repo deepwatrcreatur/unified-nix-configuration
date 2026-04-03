d0550180795e92c92a2af26a7ed5672797d4fc7d
Anwer Khan <deepwatrcreatur@gmail.com>
2026-04-02 23:51:13 -0400

    refactor(router): use cidr allowlists directly

--- modules/nixos/router/common.nix
+++ modules/nixos/router/common.nix
@@ -3,25 +3,6 @@ let
   hostsData = import ../../../lib/hosts.nix;
   routerHost = hostsData.hosts.router;
   backupHost = hostsData.hosts.router-backup;
-  wildcardFromCidr =
-    cidr:
-    let
-      parts = lib.splitString "/" cidr;
-      ip = builtins.elemAt parts 0;
-      prefixLength = lib.toInt (builtins.elemAt parts 1);
-      octets = lib.splitString "." ip;
-      fixedOctets = prefixLength / 8;
-      renderedOctets =
-        lib.imap0
-          (
-            index: octet:
-            if index < fixedOctets then octet else "*"
-          )
-          octets;
-    in
-    lib.concatStringsSep "." renderedOctets;
-  lanWildcard = wildcardFromCidr hostsData.networks.lan.cidr;
-  managementWildcard = wildcardFromCidr hostsData.networks.management.cidr;
 in
 {
   options.router.topology = lib.mkOption {
@@ -41,7 +22,7 @@ in
   # Shared defaults for router and router-backup.
   config.services.router-homelab = {
     enable = lib.mkDefault true;
-    netdataAllowConnectionsFrom = lib.mkDefault "${lanWildcard} ${managementWildcard}";
+    netdataAllowConnectionsFrom = lib.mkDefault "${hostsData.networks.lan.cidr} ${hostsData.networks.management.cidr}";
   };
 
   config.router.monitoring = {
