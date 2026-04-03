cda93d77ccf2b7c73328705aac684e7abf90d8e8
Anwer Khan <deepwatrcreatur@gmail.com>
2026-04-02 20:45:40 -0400

    refactor(attic): use upstream home-manager modules

--- modules/home-manager/attic-client-darwin.nix
+++ modules/home-manager/attic-client-darwin.nix
@@ -1,10 +1,5 @@
-{ config, lib, ... }:
+{ inputs, ... }:
 
 {
-  # attic-client service is only available on NixOS/Linux systems
-  # On macOS, we use the attic-client package directly without a service
-  programs.attic-client.enable = lib.mkDefault true;
-
-  # Enable user Nix configuration for Determinate Nix systems
-  services.nix-user-config.enable = lib.mkDefault true;
+  imports = [ inputs.nix-attic-infra.homeManagerModules.attic-client-darwin ];
 }
--- modules/home-manager/common/attic-client.nix
+++ modules/home-manager/common/attic-client.nix
@@ -1,7 +1,6 @@
 {
   config,
   lib,
-  pkgs,
   inputs,
   ...
 }:
@@ -9,121 +8,44 @@
 let
   cfg = config.programs.attic-client;
   atticCache = import ../../../lib/attic-cache.nix;
+  atticServerType = lib.types.submodule {
+    options = {
+      endpoint = lib.mkOption {
+        type = lib.types.str;
+        description = "Attic server endpoint URL";
+      };
+
+      tokenPath = lib.mkOption {
+        type = lib.types.str;
+        default = "${config.home.homeDirectory}/.config/sops/attic-client-token";
+        description = "Path to the token file used for this server.";
+      };
+
+      aliases = lib.mkOption {
+        type = lib.types.listOf lib.types.str;
+        default = [ ];
+        description = "Optional attic push/pull alias names for this server.";
+      };
+    };
+  };
 in
 {
-  options.programs.attic-client = {
-    enable = lib.mkEnableOption "Attic binary cache client" // {
-      default = false;
-      description = "Whether to enable Attic binary cache client with SOPS-managed authentication";
-    };
+  imports = [ inputs.nix-attic-infra.homeManagerModules.attic-client ];
 
-    servers = lib.mkOption {
-      type = lib.types.attrsOf (
-        lib.types.submodule {
-          options = {
-            endpoint = lib.mkOption {
-              type = lib.types.str;
-              description = "Attic server endpoint URL";
-            };
-            tokenPath = lib.mkOption {
-              type = lib.types.str;
-              default = "${config.home.homeDirectory}/.config/sops/attic-client-token";
-              description = "Path to the SOPS-decrypted token file";
-            };
-          };
-        }
-      );
-      default = { };
-      description = "Attic servers configuration";
-    };
-
-    defaultServers = lib.mkOption {
-      type = lib.types.attrsOf (
-        lib.types.submodule {
-          options = {
-            endpoint = lib.mkOption {
-              type = lib.types.str;
-              description = "Attic server endpoint URL";
-            };
-            tokenPath = lib.mkOption {
-              type = lib.types.str;
-              default = "${config.home.homeDirectory}/.config/sops/attic-client-token";
-              description = "Path to the SOPS-decrypted token file";
-            };
-          };
-        }
-      );
-      default = {
-        ${atticCache.serverName} = {
-          endpoint = atticCache.serverEndpoint;
-        };
-        "${atticCache.serverName}-local" = {
-          endpoint = atticCache.serverEndpoint;
-        };
+  options.programs.attic-client.defaultServers = lib.mkOption {
+    type = lib.types.attrsOf atticServerType;
+    default = {
+      ${atticCache.serverName} = {
+        endpoint = atticCache.serverEndpoint;
       };
-      description = "Default Attic servers (can be overridden)";
+      "${atticCache.serverName}-local" = {
+        endpoint = atticCache.serverEndpoint;
+      };
     };
+    description = "Repository default Attic servers layered onto the upstream module.";
   };
 
   config = lib.mkIf cfg.enable {
-    # Install attic-client
-    home.packages = [ (if pkgs ? attic-fnox then pkgs.attic-fnox else pkgs.attic-client) ];
-
-    # Merge default servers with user-specified servers
     programs.attic-client.servers = lib.mkDefault cfg.defaultServers;
-
-    # Create Attic client configuration template
-    home.file.".config/attic/config.toml".text =
-      let
-        allServers = cfg.defaultServers // cfg.servers;
-        serverConfigs = lib.mapAttrsToList (name: server: ''
-          [servers.${name}]
-          endpoint = "${server.endpoint}"
-          token = "@ATTIC_CLIENT_TOKEN_${lib.toUpper (builtins.replaceStrings [ "-" ] [ "_" ] name)}@"
-        '') allServers;
-      in
-      lib.concatStringsSep "\n\n" serverConfigs;
-
-    # Home activation script to substitute tokens
-    home.activation.attic-config = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-      $DRY_RUN_CMD mkdir -p ${config.home.homeDirectory}/.config/attic
-
-      if [[ -f ${config.home.homeDirectory}/.config/attic/config.toml ]]; then
-        config_file="${config.home.homeDirectory}/.config/attic/config.toml"
-        temp_file="/tmp/attic-config-$$.toml"
-
-        # Copy the template
-        cp "$config_file" "$temp_file"
-
-        # Try to get token from fnox
-        fnox_token=$(fnox get ATTIC_CLIENT_JWT_TOKEN 2>/dev/null || echo "")
-
-        ${lib.concatStringsSep "\n        " (
-          lib.mapAttrsToList (name: server: ''
-            # Substitute token for ${name}
-            placeholder="@ATTIC_CLIENT_TOKEN_${lib.toUpper (builtins.replaceStrings [ "-" ] [ "_" ] name)}@"
-
-            if [[ -n "$fnox_token" ]]; then
-               $DRY_RUN_CMD sed -i "s|$placeholder|$fnox_token|g" "$temp_file"
-            elif [[ -f "${server.tokenPath}" ]]; then
-              token=$(cat "${server.tokenPath}")
-              $DRY_RUN_CMD sed -i "s|$placeholder|$token|g" "$temp_file"
-            else
-              $VERBOSE_ECHO "Warning: Token not found in fnox or file for ${name}: ${server.tokenPath}"
-            fi
-          '') (cfg.defaultServers // cfg.servers)
-        )}
-
-        # Move the configured file into place
-        $DRY_RUN_CMD mv "$temp_file" "$config_file"
-        $VERBOSE_ECHO "Attic client configuration updated with tokens"
-      fi
-    '';
-
-    # Add shell aliases for convenience
-    home.shellAliases = {
-      attic-push-local = "attic push cache-local";
-      attic-push-cache = "attic push attic-cache";
-    };
   };
 }
