7867c5047bf19e6676268018bd5702cbcbb63ce6
Anwer Khan <deepwatrcreatur@gmail.com>
2026-04-02 20:53:47 -0400

    fix(attic): preserve darwin defaults and aliases

--- modules/home-manager/attic-client-darwin.nix
+++ modules/home-manager/attic-client-darwin.nix
@@ -1,5 +1,8 @@
-{ inputs, ... }:
+{ inputs, lib, ... }:
 
 {
   imports = [ inputs.nix-attic-infra.homeManagerModules.attic-client-darwin ];
+
+  programs.attic-client.enable = lib.mkDefault true;
+  services.nix-user-config.enable = lib.mkDefault true;
 }
--- modules/home-manager/common/attic-client.nix
+++ modules/home-manager/common/attic-client.nix
@@ -8,44 +8,24 @@
 let
   cfg = config.programs.attic-client;
   atticCache = import ../../../lib/attic-cache.nix;
-  atticServerType = lib.types.submodule {
-    options = {
-      endpoint = lib.mkOption {
-        type = lib.types.str;
-        description = "Attic server endpoint URL";
-      };
-
-      tokenPath = lib.mkOption {
-        type = lib.types.str;
-        default = "${config.home.homeDirectory}/.config/sops/attic-client-token";
-        description = "Path to the token file used for this server.";
-      };
-
-      aliases = lib.mkOption {
-        type = lib.types.listOf lib.types.str;
-        default = [ ];
-        description = "Optional attic push/pull alias names for this server.";
-      };
+  defaultTokenPath = "${config.home.homeDirectory}/.config/sops/attic-client-token";
+  defaultServers = {
+    ${atticCache.serverName} = {
+      endpoint = atticCache.serverEndpoint;
+      tokenPath = defaultTokenPath;
+      aliases = [ "cache" ];
+    };
+    "${atticCache.serverName}-local" = {
+      endpoint = atticCache.serverEndpoint;
+      tokenPath = defaultTokenPath;
+      aliases = [ "local" ];
     };
   };
 in
 {
   imports = [ inputs.nix-attic-infra.homeManagerModules.attic-client ];
 
-  options.programs.attic-client.defaultServers = lib.mkOption {
-    type = lib.types.attrsOf atticServerType;
-    default = {
-      ${atticCache.serverName} = {
-        endpoint = atticCache.serverEndpoint;
-      };
-      "${atticCache.serverName}-local" = {
-        endpoint = atticCache.serverEndpoint;
-      };
-    };
-    description = "Repository default Attic servers layered onto the upstream module.";
-  };
-
   config = lib.mkIf cfg.enable {
-    programs.attic-client.servers = lib.mkDefault cfg.defaultServers;
+    programs.attic-client.servers = lib.mkDefault defaultServers;
   };
 }
