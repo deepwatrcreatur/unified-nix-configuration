e65bc268f3d1e93ed11d2255e3ceb844492249f4
Anwer Khan <deepwatrcreatur@gmail.com>
2026-04-02 23:13:40 -0400

    refactor(iperf3): require explicit bind probe addresses

--- den/aspects/homeserver-iperf3.nix
+++ den/aspects/homeserver-iperf3.nix
@@ -1,9 +1,15 @@
 { ... }:
+let
+  hostsData = import ../../lib/hosts.nix;
+in
 { ... }:
 {
   imports = [
     ../../modules/nixos/services/iperf3.nix
   ];
 
-  host.services.iperf3.enable = true;
+  host.services.iperf3 = {
+    enable = true;
+    bindProbeAddress = hostsData.hosts.homeserver.ip;
+  };
 }
