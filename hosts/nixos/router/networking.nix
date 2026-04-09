e726f3a3e620560552e71eb7580d12086715d9c2
Anwer Khan <deepwatrcreatur@gmail.com>
2026-04-09 03:21:42 -0400

    router: declare technitium lan scope and rotate api token

--- hosts/nixos/router/networking.nix
+++ hosts/nixos/router/networking.nix
@@ -26,6 +26,25 @@ in
     };
   };
 
+  services.router-technitium.scopes = {
+    LAN = {
+      legacyNames = [ "Default" ];
+      startingAddress = "10.10.10.100";
+      endingAddress = "10.10.10.250";
+      subnetMask = "255.255.0.0";
+      routerAddress = routerHost.ip;
+      domainName = topology.domain;
+      domainSearchList = [ topology.domain ];
+      useThisDnsServer = true;
+      ntpServers = [ routerHost.ip ];
+      # Many Wi-Fi and mobile clients now use locally administered/randomized
+      # MACs; blocking them causes silent "no lease" failures.
+      blockLocallyAdministeredMacAddresses = false;
+      ignoreClientIdentifierOption = true;
+      enabled = true;
+    };
+  };
+
   # NAT is handled by nftables (see nftables.nix)
   networking.nat.enable = false;
 }
