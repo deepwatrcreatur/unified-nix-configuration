3b56b06d3dd16364369d1985824a0c5e4d4acc21
Anwer Khan <deepwatrcreatur@gmail.com>
2026-04-25 14:39:25 -0400

    fix(iventoy): switch to Type=simple and disable daemon mode
    # Conflicts:
    #	modules/nixos/services/iventoy.nix

--- modules/nixos/services/iventoy.nix
+++ modules/nixos/services/iventoy.nix
@@ -107,12 +107,16 @@ in
       '';
 
       serviceConfig = {
-        Type = "forking";
+        Type = "simple";
         WorkingDirectory = cfg.stateDir;
-        PIDFile = "/var/run/iventoy.pid";
-        ExecStart = "${pkgs.bash}/bin/bash ${cfg.stateDir}/iventoy.sh -R start";
-        ExecStop = "${pkgs.bash}/bin/bash ${cfg.stateDir}/iventoy.sh stop";
+        Environment = [
+          "IVENTOY_API_ALL=1"
+          "IVENTOY_AUTO_RUN=1"
+          "IVENTOY_NO_DAEMON_MODE=1"
+        ];
+        ExecStart = "${pkgs.glibc}/lib/ld-linux-x86-64.so.2 --library-path ${cfg.stateDir}/lib/lin64 ${cfg.stateDir}/lib/iventoy";
         Restart = "on-failure";
+        RestartSec = "10s";
       };
     };
 
