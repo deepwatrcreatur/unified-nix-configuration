3e5a0b6b56d2f9bee8d3d0477fd47c19b9181c7f
Anwer Khan <deepwatrcreatur@gmail.com>
2026-06-20 03:29:54 -0400

    fix(locale): migrate shared locales to extraLocales

--- modules/nixos/common/locale.nix
+++ modules/nixos/common/locale.nix
@@ -7,5 +7,8 @@
 
 {
   i18n.defaultLocale = lib.mkDefault "en_US.UTF-8";
-  i18n.supportedLocales = [ "en_US.UTF-8/UTF-8" ];
+  i18n.extraLocales = [
+    "en_US.UTF-8/UTF-8"
+    "en_CA.UTF-8/UTF-8"
+  ];
 }
