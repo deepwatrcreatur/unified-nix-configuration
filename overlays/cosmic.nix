1b56691e19fce1e89b5087e1276bb3ed1088c302
Anwer Khan <deepwatrcreatur@gmail.com>
2026-07-31 09:11:47 -0400

    fix(cosmic): pass libdisplay-info.dev to cosmic-comp nativeBuildInputs

--- overlays/cosmic.nix
+++ overlays/cosmic.nix
@@ -16,7 +16,7 @@
       });
     });
     cosmic-comp = prev.cosmic-comp.overrideAttrs (old: {
-      nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ [ prev.libdisplay-info prev.pkg-config ];
+      nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ [ prev.pkg-config prev.libdisplay-info.dev ];
       buildInputs = (old.buildInputs or [ ]) ++ [ prev.libdisplay-info ];
     });
   })
