94ffe38b242410a2e67bfcac02e3f1ef84929750
Anwer Khan <deepwatrcreatur@gmail.com>
2026-08-03 10:51:48 -0400

    fix(router): update caddy vendor hash

--- hosts/nixos/router/caddy.nix
+++ hosts/nixos/router/caddy.nix
@@ -42,7 +42,7 @@ in
       plugins = [
         "github.com/caddy-dns/cloudflare@v0.2.3"
       ];
-      hash = "sha256-LEpsjwy0CYx04cg42CfG6/sFv86kHmhezUG6yGedYcA=";
+      hash = "sha256-to0fhW7LWBocw1ccpPQ7e2nod7iJO9gkWZpjHsZDeu4=";
     };
     environmentFile = "/run/caddy/caddy.env";
 
