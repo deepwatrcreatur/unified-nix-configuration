dfc57f6afa1473a9fb48a2eeb768e2fe8617fe95
Anwer Khan <deepwatrcreatur@gmail.com>
2026-09-23 04:57:44 -0400

    fix(emerald): fix DNS and asymmetric routing caused by dual-NIC DHCP and ARP flux

--- hosts/nixos/emerald/networking.nix
+++ hosts/nixos/emerald/networking.nix
@@ -16,10 +16,15 @@
           MACAddress = "34:5a:60:01:5e:f8";
         };
         networkConfig = {
-          DHCP = "yes";
+          DHCP = "ipv6";
           IPv6AcceptRA = true;
           MulticastDNS = true;
-          DNS = [ "10.10.10.1" ];
+          DNS = [
+            "10.10.10.1"
+            "1.1.1.1"
+            "8.8.8.8"
+          ];
+          Domains = [ "deepwatercreature.com" ];
         };
         address = [
           "10.10.11.55/16"
@@ -31,25 +36,27 @@
             Metric = 100;
           }
         ];
-        dhcpV4Config = {
-          RouteMetric = 100;
-          UseDNS = true;
-          UseRoutes = true;
-        };
         linkConfig = {
           RequiredForOnline = lib.mkForce "routable";
         };
       };
 
-      # Deprioritize any secondary physical ethernet interfaces (e.g. 10Gb card before replacement with GPU)
+      # Deprioritize/disable secondary physical ethernet interfaces (e.g. 10Gb card before replacement with GPU)
+      # to prevent ARP flux and conflicting DHCP leases on 10.10.0.0/16
       "50-ethernet" = {
-        dhcpV4Config = {
-          RouteMetric = lib.mkForce 1024;
+        networkConfig = {
+          DHCP = lib.mkForce "no";
+          IPv6AcceptRA = lib.mkForce false;
         };
         linkConfig = {
           RequiredForOnline = lib.mkForce "no";
         };
       };
     };
+  };
+
+  boot.kernel.sysctl = {
+    "net.ipv4.conf.all.arp_ignore" = 1;
+    "net.ipv4.conf.all.arp_announce" = 2;
   };
 }
