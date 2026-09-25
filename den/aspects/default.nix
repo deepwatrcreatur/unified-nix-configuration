57706619743d768f8b88744fcaf78ad85fbba27b
Anwer Khan <deepwatrcreatur@gmail.com>
2026-09-25 01:15:31 -0400

    feat(specializations): implement deep-focus bootloader specialization aspect

--- den/aspects/default.nix
+++ den/aspects/default.nix
@@ -36,4 +36,5 @@
   desktop-gnome = context: import ./desktop-gnome.nix context;
   hardware-printer-phoenix-m477 = context: import ./hardware-printer-phoenix-m477.nix context;
   btrfs-snapper = context: import ./btrfs-snapper.nix context;
+  deep-focus = context: import ./deep-focus.nix context;
 }
